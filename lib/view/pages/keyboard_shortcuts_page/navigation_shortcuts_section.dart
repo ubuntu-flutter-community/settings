05154f94cd965c5e49e7427baddd6ba792d69f06
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-07-06 01:11:55 +0200

    Fix name and organize imports

--- lib/view/pages/keyboard_shortcuts_page/navigation_shortcuts_section.dart
+++ lib/view/pages/keyboard_shortcuts_page/navigation_shortcuts_section.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gsettings/gsettings.dart';
 import 'package:settings/view/pages/keyboard_shortcuts_page/keyboard_shortcut_row.dart';
-import 'package:settings/view/widgets/settings_row.dart';
 import 'package:settings/view/widgets/settings_section.dart';
 
 class NavigationShortcutsSection extends StatefulWidget {
@@ -16,17 +14,18 @@ class NavigationShortcutsSection extends StatefulWidget {
 
 class _NavigationShortcutsSectionState
     extends State<NavigationShortcutsSection> {
-  late GSettings _settings;
+  late GSettings _settingsWmShortcuts;
 
   @override
   void initState() {
-    _settings = GSettings(schemaId: 'org.gnome.desktop.wm.keybindings');
+    _settingsWmShortcuts =
+        GSettings(schemaId: 'org.gnome.desktop.wm.keybindings');
     super.initState();
   }
 
   @override
   void dispose() {
-    _settings.dispose();
+    _settingsWmShortcuts.dispose();
     super.dispose();
   }
 
@@ -34,7 +33,8 @@ class _NavigationShortcutsSectionState
   Widget build(BuildContext context) {
     return SettingsSection(headline: 'Navigation Shortcuts', children: [
       KeyboardShortcutRow(
-          schemaId: _settings.schemaId, settingsKey: 'switch-windows')
+          schemaId: _settingsWmShortcuts.schemaId,
+          settingsKey: 'switch-windows')
     ]);
   }
 }
