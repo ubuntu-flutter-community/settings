cd2bd2818d4f93e0b939c4c8ffa5fd48fd31a01f
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-07-08 14:27:28 +0200

    Add a really primitive test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,27 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:gsettings/gsettings.dart';
+import 'package:provider/provider.dart';
 import 'package:settings/main.dart';
+import 'package:settings/view/widgets/app_theme.dart';
 
 void main() {
+  const _schemaKey = 'org.gnome.desktop.interface';
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const UbuntuSettingsApp());
+    if (GSettingsSchema.lookup(_schemaKey) != null) {
+      final _themeSettings = GSettings(schemaId: _schemaKey);
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => AppTheme(_themeSettings),
+          child: const MaterialApp(
+            home: UbuntuSettingsApp(),
+          ),
+        ),
+      );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      expect(find.byType(AppBar), findsNWidgets(1));
+    }
   });
 }
