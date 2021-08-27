0cd4286ca57ed5e50cabcc2b04ea5b879935c990
Michal Predotka <mivoligo@gmail.com>
2021-08-27 19:28:22 +0100

    Add simple settings to Typing section

--- lib/view/pages/accessibility/typing_section.dart
+++ lib/view/pages/accessibility/typing_section.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:settings/view/widgets/settings_section.dart';
+import 'package:settings/view/widgets/single_gsetting_row.dart';
 
 class TypingSection extends StatelessWidget {
   const TypingSection({Key? key}) : super(key: key);
 
+  final _schemaA11yApps = 'org.gnome.desktop.a11y.applications';
+
   @override
   Widget build(BuildContext context) {
-    return SettingsSection(headline: 'Typing', children: []);
+    return SettingsSection(
+      headline: 'Typing',
+      children: [
+        SingleGsettingRow(
+          actionLabel: 'Screen Keyboard',
+          schemaId: _schemaA11yApps,
+          settingsKey: 'screen-keyboard-enabled',
+        ),
+      ],
+    );
   }
 }
