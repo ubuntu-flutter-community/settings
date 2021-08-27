867b53609d081cd77767dfa37db2ffe74bb13fe8
Michal Predotka <mivoligo@gmail.com>
2021-08-27 17:32:46 +0100

    Add simple settings to Seeing section

--- lib/view/pages/accessibility/seeing_section.dart
+++ lib/view/pages/accessibility/seeing_section.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:settings/view/widgets/settings_section.dart';
+import 'package:settings/view/widgets/single_gsetting_row.dart';
 
 class SeeingSection extends StatelessWidget {
   const SeeingSection({Key? key}) : super(key: key);
 
+  final _schemaA11yApps = 'org.gnome.desktop.a11y.applications';
+  final _schemaA11yKeyboard = 'org.gnome.desktop.a11y.keyboard';
+
   @override
   Widget build(BuildContext context) {
-    return SettingsSection(headline: 'Seeing', children: []);
+    return SettingsSection(
+      headline: 'Seeing',
+      children: [
+        SingleGsettingRow(
+          actionLabel: 'Screen Reader',
+          settingsKey: 'screen-reader-enabled',
+          schemaId: _schemaA11yApps,
+        ),
+        SingleGsettingRow(
+          actionLabel: 'Sound Keys',
+          actionDescription:
+              'Beep when Num Lock or Caps Lock are turned on or off',
+          settingsKey: 'togglekeys-enable',
+          schemaId: _schemaA11yKeyboard,
+        ),
+      ],
+    );
   }
 }
