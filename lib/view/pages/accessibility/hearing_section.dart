109ad534b6b1202d8ecb0a66ba23147a96fa46f0
Michal Predotka <mivoligo@gmail.com>
2021-09-02 20:26:33 +0100

    Use new custom widget in Hearing and Typing sections

--- lib/view/pages/accessibility/hearing_section.dart
+++ lib/view/pages/accessibility/hearing_section.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:settings/view/pages/accessibility/accessibility_model.dart';
-import 'package:settings/view/widgets/settings_row.dart';
+import 'package:settings/view/widgets/extra_options_gsettings_row.dart';
 import 'package:settings/view/widgets/settings_section.dart';
-import 'package:settings/view/widgets/switch_settings_row.dart';
 
 class HearingSection extends StatelessWidget {
   const HearingSection({Key? key}) : super(key: key);
@@ -25,20 +24,17 @@ class _VisualAlerts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _model = Provider.of<AccessibilityModel>(context);
-    return InkWell(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (_) => ChangeNotifierProvider.value(
-            value: _model,
-            child: const _VisualAlertsSettings(),
-          ),
-        );
-      },
-      child: SettingsRow(
-        actionLabel: 'Visual Alerts',
-        secondChild:
-            _model.getVisualAlerts ? const Text('On') : const Text('Off'),
+    return ExtraOptionsGsettingsRow(
+      actionLabel: 'Visual Alerts',
+      actionDescription: 'Use a visual indication when an alert sound occurs',
+      value: _model.getVisualAlerts,
+      onChanged: (value) => _model.setVisualAlerts(value),
+      onPressed: () => showDialog(
+        context: context,
+        builder: (_) => ChangeNotifierProvider.value(
+          value: _model,
+          child: const _VisualAlertsSettings(),
+        ),
       ),
     );
   }
@@ -54,30 +50,17 @@ class _VisualAlertsSettings extends StatelessWidget {
       title: const Center(child: Text('Visual Alerts')),
       contentPadding: const EdgeInsets.all(8.0),
       children: [
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text('Use a visual indication when an alert sound occurs.'),
-        ),
-        SwitchSettingsRow(
-          actionLabel: 'Visual Alerts',
-          value: _model.getVisualAlerts,
-          onChanged: (value) => _model.setVisualAlerts(value),
-        ),
         RadioListTile(
           title: const Text('Flash the entire window'),
           value: 'frame-flash',
           groupValue: _model.getVisualAlertsType,
-          onChanged: _model.getVisualAlerts
-              ? (String? value) => _model.setVisualAlertsType(value!)
-              : null,
+          onChanged: (String? value) => _model.setVisualAlertsType(value!),
         ),
         RadioListTile(
           title: const Text('Flash the entire screen'),
           value: 'fullscreen-flash',
           groupValue: _model.getVisualAlertsType,
-          onChanged: _model.getVisualAlerts
-              ? (String? value) => _model.setVisualAlertsType(value!)
-              : null,
+          onChanged: (String? value) => _model.setVisualAlertsType(value!),
         ),
       ],
     );
--- lib/view/pages/accessibility/typing_section.dart
+++ lib/view/pages/accessibility/typing_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:settings/view/pages/accessibility/accessibility_model.dart';
+import 'package:settings/view/widgets/extra_options_gsettings_row.dart';
 import 'package:settings/view/widgets/settings_row.dart';
 import 'package:settings/view/widgets/settings_section.dart';
 import 'package:settings/view/widgets/switch_settings_row.dart';
@@ -32,20 +33,17 @@ class _RepeatKeys extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _model = Provider.of<AccessibilityModel>(context);
-    return InkWell(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (_) => ChangeNotifierProvider.value(
-            value: _model,
-            child: const _RepeatKeysSettings(),
-          ),
-        );
-      },
-      child: SettingsRow(
-        actionLabel: 'Repeat Keys',
-        secondChild:
-            _model.getKeyboardRepeat ? const Text('On') : const Text('Off'),
+    return ExtraOptionsGsettingsRow(
+      actionLabel: 'Repeat Keys',
+      actionDescription: 'Key presses repeat when key is held down',
+      value: _model.getKeyboardRepeat,
+      onChanged: (value) => _model.setKeyboardRepeat(value),
+      onPressed: () => showDialog(
+        context: context,
+        builder: (_) => ChangeNotifierProvider.value(
+          value: _model,
+          child: const _RepeatKeysSettings(),
+        ),
       ),
     );
   }
@@ -61,12 +59,6 @@ class _RepeatKeysSettings extends StatelessWidget {
       title: const Center(child: Text('Repeat Keys')),
       contentPadding: const EdgeInsets.all(8.0),
       children: [
-        SwitchSettingsRow(
-          actionLabel: 'Repeat Keys',
-          actionDescription: 'Key presses repeat when key is held down',
-          value: _model.getKeyboardRepeat,
-          onChanged: (value) => _model.setKeyboardRepeat(value),
-        ),
         SettingsRow(
           actionLabel: 'Delay',
           actionDescription: 'Initial key repeat delay',
@@ -106,20 +98,17 @@ class _CursorBlinking extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _model = Provider.of<AccessibilityModel>(context);
-    return InkWell(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (_) => ChangeNotifierProvider.value(
-            value: _model,
-            child: const _CursorBlinkingSettings(),
-          ),
-        );
-      },
-      child: SettingsRow(
-        actionLabel: 'Cursor Blinking',
-        secondChild:
-            _model.getCursorBlink ? const Text('On') : const Text('Off'),
+    return ExtraOptionsGsettingsRow(
+      actionLabel: 'Cursor Blinking',
+      actionDescription: 'Cursor blinks in text fields',
+      value: _model.getCursorBlink,
+      onChanged: (value) => _model.setCursorBlink(value),
+      onPressed: () => showDialog(
+        context: context,
+        builder: (_) => ChangeNotifierProvider.value(
+          value: _model,
+          child: const _CursorBlinkingSettings(),
+        ),
       ),
     );
   }
@@ -135,12 +124,6 @@ class _CursorBlinkingSettings extends StatelessWidget {
       title: const Center(child: Text('Cursor Blinking')),
       contentPadding: const EdgeInsets.all(8.0),
       children: [
-        SwitchSettingsRow(
-          actionLabel: 'Cursor Blinking',
-          actionDescription: 'Cursor blinks in text fields',
-          value: _model.getCursorBlink,
-          onChanged: (value) => _model.setCursorBlink(value),
-        ),
         SettingsRow(
           actionLabel: 'Cursor Blink Time',
           actionDescription: 'Length of the cursor blink cycle',
