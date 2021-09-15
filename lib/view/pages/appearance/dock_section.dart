0244106f945120f7530e78fbc370604bd0ec3277
Michal Predotka <mivoligo@gmail.com>
2021-09-15 09:43:35 +0100

    Don't use underscore for local variable

--- lib/view/pages/appearance/dock_section.dart
+++ lib/view/pages/appearance/dock_section.dart
@@ -11,49 +11,49 @@ class DockSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _model = Provider.of<AppearanceModel>(context);
+    final model = Provider.of<AppearanceModel>(context);
 
     return SettingsSection(
       headline: 'Dock',
       children: [
         SwitchSettingsRow(
           actionLabel: 'Show Trash',
-          value: _model.getShowTrash,
-          onChanged: (value) => _model.setShowTrash(value),
+          value: model.getShowTrash,
+          onChanged: (value) => model.setShowTrash(value),
         ),
         SwitchSettingsRow(
           actionLabel: 'Always Show Dock',
-          value: _model.getAlwaysShowDock,
-          onChanged: (value) => _model.setAlwaysShowDock(value),
+          value: model.getAlwaysShowDock,
+          onChanged: (value) => model.setAlwaysShowDock(value),
         ),
         SwitchSettingsRow(
           actionLabel: 'Extend Dock',
-          value: _model.getExtendDock,
-          onChanged: (value) => _model.setExtendDock(value),
+          value: model.getExtendDock,
+          onChanged: (value) => model.setExtendDock(value),
         ),
         SwitchSettingsRow(
           actionLabel: 'Active App Glow',
-          value: _model.getAppGlow,
-          onChanged: (value) => _model.setAppGlow(value),
+          value: model.getAppGlow,
+          onChanged: (value) => model.setAppGlow(value),
         ),
         SliderSettingsRow(
           actionLabel: 'Icon Size',
-          value: _model.getMaxIconSize,
+          value: model.getMaxIconSize,
           min: 16,
           max: 64,
-          onChanged: (value) => _model.setMaxIconSize(value),
+          onChanged: (value) => model.setMaxIconSize(value),
         ),
         ToggleButtonsSettingRow(
           actionLabel: 'Dock Position',
-          selectedValues: _model.getSelectedDockPositions,
+          selectedValues: model.getSelectedDockPositions,
           labels: const ['Left', 'Right', 'Bottom'],
-          onPressed: (index) => _model.setDockPosition(index),
+          onPressed: (index) => model.setDockPosition(index),
         ),
         ToggleButtonsSettingRow(
           actionLabel: 'App Icon Click Behaviour',
-          selectedValues: _model.getSelectedClickActions,
+          selectedValues: model.getSelectedClickActions,
           labels: const ['Minimize', 'Focus or previews'],
-          onPressed: (index) => _model.setClickAction(index),
+          onPressed: (index) => model.setClickAction(index),
         ),
       ],
     );
