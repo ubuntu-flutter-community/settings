77d730c85f8ff98036d7438edfd3e1e47c11de73
Paul Kepinski <paul.kepinski@orange.fr>
2021-09-17 20:48:41 +0200

    Add many missing infos
    - Gpu name
    - Hostname
    - OS type
    - Gnome version
    - Window server

--- lib/view/pages/info/info_page.dart
+++ lib/view/pages/info/info_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:dbus/dbus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:settings/view/widgets/settings_section.dart';
@@ -6,8 +9,37 @@ import 'package:yaru_icons/widgets/yaru_icons.dart';
 import 'package:yaru/yaru.dart' as yaru;
 import 'package:linux_system_info/linux_system_info.dart';
 
-class InfoPage extends StatelessWidget {
-  const InfoPage({Key? key}) : super(key: key);
+class InfoPage extends StatefulWidget {
+  const InfoPage({ Key? key }) : super(key: key);
+
+  @override
+  _InfoPageState createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  String _gpuName = "";
+  String _hostname = "";
+
+  final DBusRemoteObject _dBusRemoteObject = DBusRemoteObject(
+    DBusClient.system(),
+    name: 'org.freedesktop.hostname1',
+    path: DBusObjectPath('/org/freedesktop/hostname1')
+  );
+
+
+  _InfoPageState() {
+    GpuInfo.load().then((gpus) {
+      setState(() {
+        _gpuName = gpus.first.model;
+      });
+    });
+    
+    _dBusRemoteObject.getProperty('org.freedesktop.hostname1', 'Hostname').then((hostname) {
+      setState(() {
+        _hostname = hostname.toNative();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +60,33 @@ class InfoPage extends StatelessWidget {
             infoLabel: 'Memory',
             infoValue: MemInfo().mem_total_gb.toString() + ' Gb'
           ),
-          const SingleInfoRow(infoLabel: 'Graphics', infoValue: 'AMD Radeon R7 370'),
+          SingleInfoRow(
+            infoLabel: 'Graphics',
+            infoValue: _gpuName
+          ),
           const SingleInfoRow(infoLabel: 'Disk Capacity', infoValue: '1 To'),
         ]),
         SettingsSection(headline: 'System', children: [
           SingleInfoRow(
+            infoLabel: 'Hostname',
+            infoValue: _hostname
+          ),
+          SingleInfoRow(
             infoLabel: 'OS name',
             infoValue: osNameVersion
           ),
-          const SingleInfoRow(infoLabel: 'OS type', infoValue: '64-bit'),
+          SingleInfoRow(
+            infoLabel: 'OS type',
+            infoValue: sizeOf<IntPtr>() == 8 ? '64 bits' : '32 bits',
+          ),
+          SingleInfoRow(
+            infoLabel: 'Gnome version',
+            infoValue: GnomeInfo().version,
+          ),
+          SingleInfoRow(
+            infoLabel: 'Window server',
+            infoValue: Platform.environment['XDG_SESSION_TYPE'] ?? ''
+          ),
         ]),
       ],
     );
