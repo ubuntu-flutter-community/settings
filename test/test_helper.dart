90c572711110a00ba9da2e671f710e13f9a54db8
Shan Shaji <abrahamshanshaji@gmail.com>
2021-09-17 20:37:57 +0530

    test: add checkbox_row test (#69)
    * test: add checkbox_row test
    
    * Add an extension method [pumpScreen] on WidgetTester

---
+++ test/test_helper.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+typedef WidgetBuilderNoContext = Widget Function();
+
+extension TesterExtension on WidgetTester {
+  /// Test Helper to instantiate and pump widget/screen which depends on Repository [R]
+  /// and optionally depends on Provider  [P]. Add non null [args], if the widgets needs arguments from
+  /// navigation arguments when it called using:
+  /// ```
+  /// Navigator.of(context).pushNamed('/path', arguments: args);
+  /// ```
+
+  Future<void> pumpScreen<R, P extends ChangeNotifier>({
+    required WidgetBuilderNoContext widgetBuilder,
+    required R repository,
+    P? provider,
+    dynamic args,
+  }) async {
+    Widget toTest = widgetBuilder.call();
+    if (args != null) {
+      toTest = Navigator(
+        onGenerateRoute: (_) {
+          return MaterialPageRoute<Widget>(
+            builder: (_) => widgetBuilder.call(),
+            settings: RouteSettings(arguments: args),
+          );
+        },
+      );
+    }
+
+    Widget parent = MaterialApp(
+      home: toTest,
+    );
+
+    if (provider != null) {
+      parent = ChangeNotifierProvider<P>.value(
+        value: provider,
+        child: parent,
+      );
+    }
+
+    await pumpWidget(
+      Provider<R>.value(
+        value: repository,
+        child: parent,
+      ),
+    );
+  }
+}
