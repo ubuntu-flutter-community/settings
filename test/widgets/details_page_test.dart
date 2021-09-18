e8e321ca093ab04a374a9abc38138d816f8b0c96
shan-shaji <abrahamshanshaji@gmail.com>
2021-09-18 23:28:04 +0530

    test: refactored code * removed unnecessary group * add comments

--- test/widgets/details_page_test.dart
+++ test/widgets/details_page_test.dart
@@ -10,80 +10,77 @@ import '../test_helper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  group('- Details Page Test', () {
-    testWidgets('- Details Page Buid Test', (widgetTester) async {
-      await widgetTester.pumpScreen(
-        repository: null,
-        widgetBuilder: () => const DetailPage(
+  testWidgets('- Details Page Build Test', (widgetTester) async {
+    await widgetTester.pumpScreen(
+      repository: null,
+      widgetBuilder: () => const DetailPage(
+        item: MenuItem(
+          name: 'WIFI',
+          iconData: YaruIcons.network_wireless,
+          details: Text('WIFI Text'),
+        ),
+      ),
+    );
+
+    expect(find.text('WIFI'), findsOneWidget);
+    expect(find.text('WIFI Text'), findsOneWidget);
+  });
+
+  testWidgets('- Appbar leading property will be null', (widgetTester) async {
+    await widgetTester.pumpScreen(
+      repository: null,
+      widgetBuilder: () => const DetailPage(
+        item: MenuItem(
+          name: 'WIFI',
+          iconData: YaruIcons.network_wireless,
+          details: Text('WIFI Text'),
+        ),
+      ),
+    );
+
+    expect(
+      find.byType(BackButton),
+      findsNothing,
+    );
+  });
+
+  testWidgets('- Appbar leading prop will be initalized with BackButton ',
+      (widgetTester) async {
+
+    /// screen size will be 414.0 * 846.0
+    /// To enable [BackButton] width is set to 400
+    const width = 400.0;
+    const height = 800.0;
+
+    widgetTester.binding.window.physicalSizeTestValue = const Size(
+      width,
+      height,
+    );
+
+    final navigator = MockNavigator();
+
+    mockito.when(() => navigator.popUntil(any()));
+
+    await widgetTester.pumpScreen(
+      navigator: navigator,
+      repository: null,
+      widgetBuilder: () => const Scaffold(
+        body: DetailPage(
           item: MenuItem(
             name: 'WIFI',
             iconData: YaruIcons.network_wireless,
             details: Text('WIFI Text'),
           ),
         ),
-      );
+      ),
+    );
 
-      expect(find.text('WIFI'), findsOneWidget);
-      expect(find.text('WIFI Text'), findsOneWidget);
-    });
+    expect(find.byType(BackButton), findsOneWidget);
+    await widgetTester.tap(find.byType(BackButton));
+    await widgetTester.pumpAndSettle();
+    verify(() => navigator.popUntil(any())).called(1);
 
-    testWidgets(
-        '- If `tablet(context)==true` the appbar [leading] will be null',
-        (widgetTester) async {
-      await widgetTester.pumpScreen(
-        repository: null,
-        widgetBuilder: () => const DetailPage(
-          item: MenuItem(
-            name: 'WIFI',
-            iconData: YaruIcons.network_wireless,
-            details: Text('WIFI Text'),
-          ),
-        ),
-      );
-
-      expect(
-        find.byType(BackButton),
-        findsNothing,
-      );
-    });
-
-    testWidgets(
-        '- If `tablet(context)==false` the appbar [leading] will contain [BackButton]',
-        (widgetTester) async {
-      const width = 414;
-      const height = 846;
-
-      widgetTester.binding.window.devicePixelRatioTestValue = (2.625);
-      widgetTester.binding.window.textScaleFactorTestValue = (1.1);
-
-      final dpi = widgetTester.binding.window.devicePixelRatio;
-      widgetTester.binding.window.physicalSizeTestValue = Size(
-        width * dpi,
-        height * dpi,
-      );
-
-      final navigator = MockNavigator();
-
-      mockito.when(() => navigator.popUntil(any()));
-
-      await widgetTester.pumpScreen(
-        navigator: navigator,
-        repository: null,
-        widgetBuilder: () => const Scaffold(
-          body: DetailPage(
-            item: MenuItem(
-              name: 'WIFI',
-              iconData: YaruIcons.network_wireless,
-              details: Text('WIFI Text'),
-            ),
-          ),
-        ),
-      );
-
-      expect(find.byType(BackButton), findsOneWidget);
-      await widgetTester.tap(find.byType(BackButton));
-      await widgetTester.pumpAndSettle();
-      verify(() => navigator.popUntil(any())).called(1);
-    });
+    // resets the screen to its orinal size after the test end
+    addTearDown(widgetTester.binding.window.clearPhysicalSizeTestValue);
   });
 }
