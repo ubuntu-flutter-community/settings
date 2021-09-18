3a777155640a5661329e3e7b653c7c3b6ce50e57
Paul Kepinski <paul.kepinski@orange.fr>
2021-09-18 23:31:29 +0200

    Get disk capacity

--- lib/view/pages/info/info_model.dart
+++ lib/view/pages/info/info_model.dart
@@ -3,23 +3,37 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:linux_system_info/linux_system_info.dart';
+import 'package:filesize/filesize.dart';
+import 'package:udisks/udisks.dart';
 
 import 'hostname_service.dart';
 
 class InfoModel extends ChangeNotifier {
-  InfoModel([HostnameService? hostnameService])
-  : _hostnameService = hostnameService ?? HostnameService();
+  InfoModel([
+    HostnameService? hostnameService,
+    UDisksClient? uDisksClient
+  ]) :
+    _hostnameService = hostnameService ?? HostnameService(),
+    _uDisksClient = uDisksClient ?? UDisksClient()
+  ;
 
   final HostnameService _hostnameService;
+  final UDisksClient _uDisksClient;
 
   String _gpuName = '';
+  String _diskCapacity = '';
 
   void init() {
     _hostnameService.init().then((_) => notifyListeners());
-    
+
     GpuInfo.load().then((gpus) {
       notifyListeners();
       _gpuName = gpus.first.model;
+    });
+
+    _uDisksClient.connect().then((value) {
+      final total = _uDisksClient.drives.fold<int>(0, (sum, drive) => sum + drive.size);
+      _diskCapacity = filesize(total);
     });
   }
 
@@ -36,7 +50,7 @@ class InfoModel extends ChangeNotifier {
   String get processor => CpuInfo.getProcessors()[0].model_name + ' x ' + (CpuInfo.getProcessors().length + 1).toString();
   String get memory => MemInfo().mem_total_gb.toString() + ' Gb';
   String get graphics => _gpuName;
-  String get diskCapacity => '';
+  String get diskCapacity => _diskCapacity;
 
   String get gnomeVersion => GnomeInfo().version;
   String get windowServer => Platform.environment['XDG_SESSION_TYPE'] ?? '';
@@ -44,6 +58,7 @@ class InfoModel extends ChangeNotifier {
   @override
   void dispose() {
     _hostnameService.dispose();
+    _uDisksClient.close();
     super.dispose();
   }
 }
