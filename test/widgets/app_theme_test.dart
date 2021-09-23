62999271341913a029746250a4ecaf970b59e90a
shan-shaji <abrahamshanshaji@gmail.com>
2021-09-24 00:56:48 +0530

    test: add app theme test

---
+++ test/widgets/app_theme_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gsettings/gsettings.dart';
+import 'package:mockito/annotations.dart';
+import 'package:settings/view/widgets/app_theme.dart';
+import 'app_theme_test.mocks.dart';
+import 'package:mockito/mockito.dart';
+
+@GenerateMocks([GSettings])
+void main() {
+  test(
+    'App Theme Dark Mode Test',
+    () {
+      GSettings settings = MockGSettings();
+      AppTheme theme = AppTheme(settings);
+
+      when(settings.setValue('gtk-theme', 'Yaru-dark')).thenAnswer(
+        (realInvocation) => true,
+      );
+
+      theme.apply(Brightness.dark);
+      verify(settings.setValue('gtk-theme', 'Yaru-dark')).called(1);
+    },
+  );
+
+  test(
+    'App Theme Light Mode Test',
+    () {
+      GSettings settings = MockGSettings();
+      AppTheme theme = AppTheme(settings);
+
+      when(settings.setValue('gtk-theme', 'Yaru')).thenAnswer(
+        (realInvocation) => true,
+      );
+
+      theme.apply(Brightness.light);
+      verify(settings.setValue('gtk-theme', 'Yaru')).called(1);
+    },
+  );
+}
