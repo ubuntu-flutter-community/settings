c503f1d790f8447587104f355d053e0c619ff2ec
J-P Nurmi <jpnurmi@gmail.com>
2021-09-27 21:20:05 +0200

    Add model for app notifications (#96)
    * Get the list of apps from NotificationsModel
    
    NotificationsModel has a GSettings object for the notifications schema.
    The list of apps can be queried from the model to avoid creating more
    instances of the same GSettings.
    
    * Add model for app notifications

--- lib/view/pages/notifications_page.dart/app_notifications_section.dart
+++ lib/view/pages/notifications_page.dart/app_notifications_section.dart
@@ -1,42 +1,46 @@
 import 'package:flutter/widgets.dart';
-import 'package:gsettings/gsettings.dart';
+import 'package:provider/provider.dart';
 import 'package:settings/schemas/schemas.dart';
+import 'package:settings/view/pages/notifications_page.dart/notifications_model.dart';
 import 'package:settings/view/widgets/settings_section.dart';
-import 'package:settings/view/widgets/single_gsetting_row.dart';
+import 'package:settings/view/widgets/switch_settings_row.dart';
 
 class AppNotificationsSection extends StatelessWidget {
   const AppNotificationsSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    late List<String?> applicationChildren;
-
-    if (GSettingsSchema.lookup(schemaNotifications) != null) {
-      applicationChildren = GSettings(schemaId: schemaNotifications)
-          .stringArrayValue('application-children');
-    }
+    final model = Provider.of<NotificationsModel>(context);
 
     return SettingsSection(
       schemaId: schemaNotifications + '.application',
       headline: 'App notifications',
-      children: GSettingsSchema.lookup(schemaNotifications) != null
-          ? applicationChildren.map(
-              (appString) {
-                const appSchemaId = schemaNotifications + '.application';
-                final path = '/' +
-                    appSchemaId.replaceAll('.', '/') +
-                    '/' +
-                    appString.toString() +
-                    '/';
-                return SingleGsettingRow(
-                  actionLabel: appString.toString(),
-                  settingsKey: 'enable',
-                  schemaId: appSchemaId,
-                  path: path,
-                );
-              },
-            ).toList()
-          : const [Text('Schema not installed ')],
+      children: model.applications
+              ?.map((appId) =>
+                  AppNotificationsSettingRow.create(context, appId: appId))
+              .toList() ??
+          const [Text('Schema not installed ')],
+    );
+  }
+}
+
+class AppNotificationsSettingRow extends StatelessWidget {
+  const AppNotificationsSettingRow({Key? key}) : super(key: key);
+
+  static Widget create(BuildContext context, {required String appId}) {
+    return ChangeNotifierProvider(
+      create: (_) => AppNotificationsModel(appId),
+      child: const AppNotificationsSettingRow(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<AppNotificationsModel>(context);
+    return SwitchSettingsRow(
+      actionLabel: model.appId,
+      value: model.enable,
+      onChanged: model.setEnable,
     );
   }
 }
