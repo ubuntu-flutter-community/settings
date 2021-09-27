178e9d6c645890088ea79feb7caca21a5bc40b1d
J-P Nurmi <jpnurmi@gmail.com>
2021-09-27 22:13:57 +0200

    Remove XxxGsettingRow (#97)
    * Remove SingleGsettingRow
    
    No longer needed. Use SwitchSettingsRow instead.
    
    * Remove ToggleButtonsGsettingRow
    
    No longer needed. Use ToggleButtonsSettingRow instead.
    
    * Remove SliderGsettingRow
    
    No longer needed. Use SliderSettingsRow instead.
    
    * SettingsSection: remove GSettings references
    
    Similar info/functionality can be later provided from models, whether
    certain functionality is available.

--- lib/view/pages/notifications_page.dart/app_notifications_section.dart
+++ lib/view/pages/notifications_page.dart/app_notifications_section.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:settings/schemas/schemas.dart';
 import 'package:settings/view/pages/notifications_page.dart/notifications_model.dart';
 import 'package:settings/view/widgets/settings_section.dart';
 import 'package:settings/view/widgets/switch_settings_row.dart';
@@ -13,7 +12,6 @@ class AppNotificationsSection extends StatelessWidget {
     final model = Provider.of<NotificationsModel>(context);
 
     return SettingsSection(
-      schemaId: schemaNotifications + '.application',
       headline: 'App notifications',
       children: model.applications
               ?.map((appId) =>
