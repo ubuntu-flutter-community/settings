1ee8671e503f1889cbd01afa2ae64a070c5cad02
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-09-29 21:49:57 +0200

    Improve model set/get

--- lib/view/pages/removable_media/removable_media_model.dart
+++ lib/view/pages/removable_media/removable_media_model.dart
@@ -64,373 +64,76 @@ class RemovableMediaModel extends SafeChangeNotifier {
     notifyListeners();
   }
 
-  // Audio CD
-  List<bool>? get getAudioStartup {
+  List<bool>? getStartup(String mimeType) {
     return [
-      _getAutoRunXContentIgnore().contains(audioCdda),
-      _getAutoRunXContentOpenFolder().contains(audioCdda),
-      _getAutoRunXContentStartApp().contains(audioCdda),
-      !_getAutoRunXContentIgnore().contains(audioCdda) &&
-          !_getAutoRunXContentOpenFolder().contains(audioCdda) &&
-          !_getAutoRunXContentStartApp().contains(audioCdda)
+      _getAutoRunXContentIgnore().contains(mimeType),
+      _getAutoRunXContentOpenFolder().contains(mimeType),
+      _getAutoRunXContentStartApp().contains(mimeType),
+      !_getAutoRunXContentIgnore().contains(mimeType) &&
+          !_getAutoRunXContentOpenFolder().contains(mimeType) &&
+          !_getAutoRunXContentStartApp().contains(mimeType)
     ];
   }
 
-  set setAudioStartup(int value) {
+  void setStartup(int value, String mimeType) {
     switch (value) {
       case 0:
-        _addAudioCddaToIgnoreList();
-        _removeAudioCddaFromFolderList();
-        _removeAudioCddaFromAppStartList();
+        _addAudioCddaToIgnoreList(mimeType);
+        _removeAudioCddaFromFolderList(mimeType);
+        _removeAudioCddaFromAppStartList(mimeType);
         break;
       case 1:
-        _addAudioCddaToFolderList();
-        _removeAudioCddaFromIgnoreList();
-        _removeAudioCddaFromAppStartList();
+        _addAudioCddaToFolderList(mimeType);
+        _removeAudioCddaFromIgnoreList(mimeType);
+        _removeAudioCddaFromAppStartList(mimeType);
         break;
       case 2:
-        _addAudioCddaToAppStartList();
-        _removeAudioCddaFromIgnoreList();
-        _removeAudioCddaFromFolderList();
+        _addAudioCddaToAppStartList(mimeType);
+        _removeAudioCddaFromIgnoreList(mimeType);
+        _removeAudioCddaFromFolderList(mimeType);
         break;
       case 3:
-        _removeAudioCddaFromIgnoreList();
-        _removeAudioCddaFromFolderList();
-        _removeAudioCddaFromAppStartList();
+        _removeAudioCddaFromIgnoreList(mimeType);
+        _removeAudioCddaFromFolderList(mimeType);
+        _removeAudioCddaFromAppStartList(mimeType);
         break;
       default:
     }
   }
 
-  void _addAudioCddaToAppStartList() {
+  void _addAudioCddaToAppStartList(String mimeType) {
     final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.add(audioCdda);
+    oldStartAppList.add(mimeType);
     _setAutoRunXContentStartApp(oldStartAppList.toList());
   }
 
-  void _removeAudioCddaFromIgnoreList() {
+  void _removeAudioCddaFromIgnoreList(String mimeType) {
     final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.remove(audioCdda);
+    oldIgnoreList.remove(mimeType);
     _setAutoRunXContentIgnore(oldIgnoreList.toList());
   }
 
-  void _addAudioCddaToFolderList() {
+  void _addAudioCddaToFolderList(String mimeType) {
     final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.add(audioCdda);
+    oldOpenFolderList.add(mimeType);
     _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
   }
 
-  void _removeAudioCddaFromAppStartList() {
+  void _removeAudioCddaFromAppStartList(String mimeType) {
     final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.remove(audioCdda);
+    oldStartAppList.remove(mimeType);
     _setAutoRunXContentStartApp(oldStartAppList.toList());
   }
 
-  void _removeAudioCddaFromFolderList() {
+  void _removeAudioCddaFromFolderList(String mimeType) {
     final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.remove(audioCdda);
+    oldOpenFolderList.remove(mimeType);
     _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
   }
 
-  void _addAudioCddaToIgnoreList() {
+  void _addAudioCddaToIgnoreList(String mimeType) {
     final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.add(audioCdda);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  // DVD-Video
-  List<bool>? get getDvdStartup {
-    return [
-      _getAutoRunXContentIgnore().contains(videoDvd),
-      _getAutoRunXContentOpenFolder().contains(videoDvd),
-      _getAutoRunXContentStartApp().contains(videoDvd),
-      !_getAutoRunXContentIgnore().contains(videoDvd) &&
-          !_getAutoRunXContentOpenFolder().contains(videoDvd) &&
-          !_getAutoRunXContentStartApp().contains(videoDvd)
-    ];
-  }
-
-  set setDvdStartup(int value) {
-    switch (value) {
-      case 0:
-        _addDvdToIgnoreList();
-        _removeDvdFromFolderList();
-        _removeDvdFromAppStartList();
-        break;
-      case 1:
-        _addDvdToFolderList();
-        _removeDvdFromIgnoreList();
-        _removeDvdFromAppStartList();
-        break;
-      case 2:
-        _addDvdToAppStartList();
-        _removeDvdFromIgnoreList();
-        _removeDvdFromFolderList();
-        break;
-      case 3:
-        _removeDvdFromIgnoreList();
-        _removeDvdFromFolderList();
-        _removeDvdFromAppStartList();
-        break;
-      default:
-    }
-  }
-
-  void _addDvdToAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.add(videoDvd);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removeDvdFromIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.remove(videoDvd);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  void _addDvdToFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.add(videoDvd);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _removeDvdFromAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.remove(videoDvd);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removeDvdFromFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.remove(videoDvd);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _addDvdToIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.add(videoDvd);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  // Musicplayer
-  List<bool>? get getMusicPlayerStartup {
-    return [
-      _getAutoRunXContentIgnore().contains(audioPlayer),
-      _getAutoRunXContentOpenFolder().contains(audioPlayer),
-      _getAutoRunXContentStartApp().contains(audioPlayer),
-      !_getAutoRunXContentIgnore().contains(audioPlayer) &&
-          !_getAutoRunXContentOpenFolder().contains(audioPlayer) &&
-          !_getAutoRunXContentStartApp().contains(audioPlayer)
-    ];
-  }
-
-  set setMusicPlayerStartup(int value) {
-    switch (value) {
-      case 0:
-        _addMusicPlayerToIgnoreList();
-        _removeMusicPlayerFromFolderList();
-        _removeMusicPlayerFromAppStartList();
-        break;
-      case 1:
-        _addMusicPlayerToFolderList();
-        _removeMusicPlayerFromIgnoreList();
-        _removeMusicPlayerFromAppStartList();
-        break;
-      case 2:
-        _addMusicPlayerToAppStartList();
-        _removeMusicPlayerFromIgnoreList();
-        _removeMusicPlayerFromFolderList();
-        break;
-      case 3:
-        _removeMusicPlayerFromIgnoreList();
-        _removeMusicPlayerFromFolderList();
-        _removeMusicPlayerFromAppStartList();
-        break;
-      default:
-    }
-  }
-
-  void _addMusicPlayerToAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.add(audioPlayer);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removeMusicPlayerFromIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.remove(audioPlayer);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  void _addMusicPlayerToFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.add(audioPlayer);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _removeMusicPlayerFromAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.remove(audioPlayer);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removeMusicPlayerFromFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.remove(audioPlayer);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _addMusicPlayerToIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.add(audioPlayer);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  // Photos
-  List<bool>? get getPhotoViewerStartup {
-    return [
-      _getAutoRunXContentIgnore().contains(imageDcf),
-      _getAutoRunXContentOpenFolder().contains(imageDcf),
-      _getAutoRunXContentStartApp().contains(imageDcf),
-      !_getAutoRunXContentIgnore().contains(imageDcf) &&
-          !_getAutoRunXContentOpenFolder().contains(imageDcf) &&
-          !_getAutoRunXContentStartApp().contains(imageDcf)
-    ];
-  }
-
-  set setPhotoViewerStartup(int value) {
-    switch (value) {
-      case 0:
-        _addPhotoViewerToIgnoreList();
-        _removePhotoViewerFromFolderList();
-        _removePhotoViewerFromAppStartList();
-        break;
-      case 1:
-        _addPhotoViewerToFolderList();
-        _removePhotoViewerFromIgnoreList();
-        _removePhotoViewerFromAppStartList();
-        break;
-      case 2:
-        _addPhotoViewerToAppStartList();
-        _removePhotoViewerFromIgnoreList();
-        _removePhotoViewerFromFolderList();
-        break;
-      case 3:
-        _removePhotoViewerFromIgnoreList();
-        _removePhotoViewerFromFolderList();
-        _removePhotoViewerFromAppStartList();
-        break;
-      default:
-    }
-  }
-
-  void _addPhotoViewerToAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.add(imageDcf);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removePhotoViewerFromIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.remove(imageDcf);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  void _addPhotoViewerToFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.add(imageDcf);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _removePhotoViewerFromAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.remove(imageDcf);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removePhotoViewerFromFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.remove(imageDcf);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _addPhotoViewerToIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.add(imageDcf);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  // Apps
-  List<bool>? get getAppsStartup {
-    return [
-      _getAutoRunXContentIgnore().contains(unixSoftware),
-      _getAutoRunXContentOpenFolder().contains(unixSoftware),
-      _getAutoRunXContentStartApp().contains(unixSoftware),
-      !_getAutoRunXContentIgnore().contains(unixSoftware) &&
-          !_getAutoRunXContentOpenFolder().contains(unixSoftware) &&
-          !_getAutoRunXContentStartApp().contains(unixSoftware)
-    ];
-  }
-
-  set setAppsStartup(int value) {
-    switch (value) {
-      case 0:
-        _addAppsToIgnoreList();
-        _removeAppsFromFolderList();
-        _removeAppsFromAppStartList();
-        break;
-      case 1:
-        _addAppsToFolderList();
-        _removeAppsFromIgnoreList();
-        _removeAppsFromAppStartList();
-        break;
-      case 2:
-        _addAppsToAppStartList();
-        _removeAppsFromIgnoreList();
-        _removeAppsFromFolderList();
-        break;
-      case 3:
-        _removeAppsFromIgnoreList();
-        _removeAppsFromFolderList();
-        _removeAppsFromAppStartList();
-        break;
-      default:
-    }
-  }
-
-  void _addAppsToAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.add(unixSoftware);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removeAppsFromIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.remove(unixSoftware);
-    _setAutoRunXContentIgnore(oldIgnoreList.toList());
-  }
-
-  void _addAppsToFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.add(unixSoftware);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _removeAppsFromAppStartList() {
-    final oldStartAppList = _getAutoRunXContentStartApp();
-    oldStartAppList.remove(unixSoftware);
-    _setAutoRunXContentStartApp(oldStartAppList.toList());
-  }
-
-  void _removeAppsFromFolderList() {
-    final oldOpenFolderList = _getAutoRunXContentOpenFolder();
-    oldOpenFolderList.remove(unixSoftware);
-    _setAutoRunXContentOpenFolder(oldOpenFolderList.toList());
-  }
-
-  void _addAppsToIgnoreList() {
-    final oldIgnoreList = _getAutoRunXContentIgnore();
-    oldIgnoreList.add(unixSoftware);
+    oldIgnoreList.add(mimeType);
     _setAutoRunXContentIgnore(oldIgnoreList.toList());
   }
 
--- lib/view/pages/removable_media/removable_media_page.dart
+++ lib/view/pages/removable_media/removable_media_page.dart
@@ -23,28 +23,33 @@ class RemovableMediaPage extends StatelessWidget {
       ToggleButtonsSettingRow(
           actionLabel: 'Audio CD',
           labels: const ['Ignore', 'Open Folder', 'Start App', 'Ask'],
-          selectedValues: model.getAudioStartup,
-          onPressed: (value) => model.setAudioStartup = value),
+          selectedValues: model.getStartup(RemovableMediaModel.audioCdda),
+          onPressed: (value) =>
+              model.setStartup(value, RemovableMediaModel.audioCdda)),
       ToggleButtonsSettingRow(
           actionLabel: 'DVD-Video',
           labels: const ['Ignore', 'Open Folder', 'Start App', 'Ask'],
-          selectedValues: model.getDvdStartup,
-          onPressed: (value) => model.setDvdStartup = value),
+          selectedValues: model.getStartup(RemovableMediaModel.videoDvd),
+          onPressed: (value) =>
+              model.setStartup(value, RemovableMediaModel.videoDvd)),
       ToggleButtonsSettingRow(
           actionLabel: 'Musicplayer',
           labels: const ['Ignore', 'Open Folder', 'Start App', 'Ask'],
-          selectedValues: model.getMusicPlayerStartup,
-          onPressed: (value) => model.setMusicPlayerStartup = value),
+          selectedValues: model.getStartup(RemovableMediaModel.audioPlayer),
+          onPressed: (value) =>
+              model.setStartup(value, RemovableMediaModel.audioPlayer)),
       ToggleButtonsSettingRow(
           actionLabel: 'Photos',
           labels: const ['Ignore', 'Open Folder', 'Start App', 'Ask'],
-          selectedValues: model.getPhotoViewerStartup,
-          onPressed: (value) => model.setPhotoViewerStartup = value),
+          selectedValues: model.getStartup(RemovableMediaModel.imageDcf),
+          onPressed: (value) =>
+              model.setStartup(value, RemovableMediaModel.imageDcf)),
       ToggleButtonsSettingRow(
           actionLabel: 'Applications',
           labels: const ['Ignore', 'Open Folder', 'Start App', 'Ask'],
-          selectedValues: model.getAppsStartup,
-          onPressed: (value) => model.setAppsStartup = value),
+          selectedValues: model.getStartup(RemovableMediaModel.unixSoftware),
+          onPressed: (value) =>
+              model.setStartup(value, RemovableMediaModel.unixSoftware)),
       CheckboxRow(
           enabled: true,
           value: model.autoRunNever,
