011fd1814fdd32a709d43020f73c0491b9251a3e
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-10-11 10:29:09 +0200

    use dropdowns for docksection

--- lib/view/pages/appearance/dock_section.dart
+++ lib/view/pages/appearance/dock_section.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:settings/view/pages/appearance/appearance_model.dart';
+import 'package:settings/view/widgets/settings_row.dart';
 import 'package:settings/view/widgets/settings_section.dart';
 import 'package:settings/view/widgets/slider_settings_row.dart';
 import 'package:settings/view/widgets/switch_settings_row.dart';
-import 'package:settings/view/widgets/toggle_buttons_setting_row.dart';
 
 class DockSection extends StatelessWidget {
   const DockSection({Key? key}) : super(key: key);
@@ -44,17 +44,29 @@ class DockSection extends StatelessWidget {
           defaultValue: 48,
           onChanged: (value) => model.setMaxIconSize(value),
         ),
-        ToggleButtonsSettingRow(
-          actionLabel: 'Dock Position',
-          selectedValues: model.selectedDockPositions,
-          labels: const ['Left', 'Right', 'Bottom'],
-          onPressed: (index) => model.setDockPosition(index),
+        SettingsRow(
+          trailingWidget: const Text('Dock Position'),
+          actionWidget: DropdownButton<String>(
+            onChanged: (value) => model.dockPosition = value,
+            value: model.dockPosition,
+            items: [
+              for (var item in AppearanceModel.dockPositions)
+                DropdownMenuItem(child: Text(item.toLowerCase()), value: item)
+            ],
+          ),
         ),
-        ToggleButtonsSettingRow(
-          actionLabel: 'App Icon Click Behaviour',
-          selectedValues: model.selectedClickActions,
-          labels: const ['Minimize', 'Focus or previews'],
-          onPressed: (index) => model.setClickAction(index),
+        SettingsRow(
+          trailingWidget: const Text('Click Action'),
+          actionWidget: DropdownButton<String>(
+            onChanged: (value) => model.clickAction = value,
+            value: model.clickAction,
+            items: [
+              for (var item in AppearanceModel.clickActions)
+                DropdownMenuItem(
+                    child: Text(item.toLowerCase().replaceAll('-', ' ')),
+                    value: item)
+            ],
+          ),
         ),
       ],
     );
