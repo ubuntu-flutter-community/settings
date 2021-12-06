7d1f7d1bff1d45f01a4614ba899593ef5ab2e679
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-12-06 11:41:04 +0100

    Add discovery to model

--- lib/view/pages/bluetooth/bluetooth_model.dart
+++ lib/view/pages/bluetooth/bluetooth_model.dart
@@ -1,19 +1,39 @@
 import 'package:bluez/bluez.dart';
-import 'package:safe_change_notifier/safe_change_notifier.dart';
+import 'package:settings/view/pages/connections/models/property_stream_notifier.dart';
 
-class BluetoothModel extends SafeChangeNotifier {
-  late BlueZClient client;
+class BluetoothModel extends PropertyStreamNotifier {
+  late final BlueZClient _client;
 
-  BluetoothModel({required this.client});
+  BluetoothModel(this._client);
 
   Future<List<BlueZDevice>> get devices async {
-    await client.connect();
-    return client.devices;
+    await _client.connect();
+    return _client.devices;
   }
 
-  Stream<BlueZDevice> deviceStream() {
-    Stream<BlueZDevice> deviceStream = client.deviceAdded;
+  void startScan() async {
+    await _client.connect().then((value) {
+      for (var adapter in _client.adapters) {
+        adapter.startDiscovery();
+      }
+    });
+  }
 
+  void stopScan() async {
+    for (var adapter in _client.adapters) {
+      await adapter.stopDiscovery();
+    }
+  }
+
+  @override
+  void dispose() async {
+    stopScan();
+    await _client.close();
+    super.dispose();
+  }
+
+  Stream<BlueZDevice> devicesAdded() {
+    Stream<BlueZDevice> deviceStream = _client.deviceAdded;
     return deviceStream;
   }
 }
--- lib/view/pages/bluetooth/bluetooth_page.dart
+++ lib/view/pages/bluetooth/bluetooth_page.dart
@@ -5,61 +5,76 @@ import 'package:settings/view/pages/bluetooth/bluetooth_device_row.dart';
 import 'package:settings/view/pages/bluetooth/bluetooth_model.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
-class BluetoothPage extends StatelessWidget {
+class BluetoothPage extends StatefulWidget {
   const BluetoothPage({Key? key}) : super(key: key);
 
   static Widget create(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => BluetoothModel(client: context.read<BlueZClient>()),
+      create: (_) => BluetoothModel(context.read<BlueZClient>()),
       child: const BluetoothPage(),
     );
   }
 
   @override
-  Widget build(BuildContext context) {
+  State<BluetoothPage> createState() => _BluetoothPageState();
+}
+
+class _BluetoothPageState extends State<BluetoothPage> {
+  @override
+  void initState() {
     final model = context.read<BluetoothModel>();
-    model.deviceStream().listen(
-      (event) {
-        print(event.address + event.name);
-      },
-    );
-    List<Widget> children = [];
+    model.startScan();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    final model = context.read<BluetoothModel>();
+    model.stopScan();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<BluetoothModel>(context, listen: true);
+    List<Widget> deviceRows = [];
     return Column(
       children: [
-        SizedBox(
-          width: 500,
-          child: YaruSection(headline: 'Bluetooth devices', children: [
-            FutureBuilder<List<BlueZDevice>>(
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  children = snapshot.data!.map((device) {
-                    return BluetoothDeviceRow(device: device);
-                  }).toList();
+        YaruSection(headline: 'Known devices', children: [
+          FutureBuilder<List<BlueZDevice>>(
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                deviceRows = snapshot.data!.map((device) {
+                  return BluetoothDeviceRow(device: device);
+                }).toList();
 
-                  return ListView(
-                    shrinkWrap: true,
-                    children: children,
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-              future: model.devices,
-            )
-          ]),
-        ),
-        SizedBox(
-          width: 500,
-          child: StreamBuilder<BlueZDevice>(
-            stream: model.deviceStream(),
-            builder: (context, deviceSnapshot) {
-              if (deviceSnapshot.hasData) {
-                children.add(BluetoothDeviceRow(device: deviceSnapshot.data!));
+                return ListView(
+                  shrinkWrap: true,
+                  children: deviceRows,
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
               }
-              return const Center(child: CircularProgressIndicator());
             },
-          ),
-        )
+            future: model.devices,
+          )
+        ]),
+        YaruSection(headline: 'New devices', children: [
+          StreamBuilder<BlueZDevice>(
+            stream: model.devicesAdded(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                BluetoothDeviceRow(
+                  device: snapshot.data!,
+                );
+              }
+              return const Padding(
+                padding: EdgeInsets.all(28.0),
+                child: CircularProgressIndicator(),
+              );
+            },
+          )
+        ]),
       ],
     );
   }
