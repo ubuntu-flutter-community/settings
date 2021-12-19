4412ca116d694ae9bbd36a45930ffd3cad72358d
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-12-19 14:13:22 +0100

    use tabs for keyboard page

--- lib/view/pages/keyboard/keyboard_page.dart
+++ lib/view/pages/keyboard/keyboard_page.dart
@@ -7,7 +7,7 @@ import 'package:settings/view/pages/keyboard/keyboard_shortcuts_page.dart';
 import 'package:yaru_icons/widgets/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
-class KeyboardPage extends StatelessWidget {
+class KeyboardPage extends StatefulWidget {
   const KeyboardPage({Key? key}) : super(key: key);
 
   static Widget create(BuildContext context) {
@@ -15,8 +15,62 @@ class KeyboardPage extends StatelessWidget {
   }
 
   @override
+  State<KeyboardPage> createState() => _KeyboardPageState();
+}
+
+class _KeyboardPageState extends State<KeyboardPage>
+    with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final service = Provider.of<SettingsService>(context);
+    return Column(
+      children: [
+        Container(
+          width: 516,
+          height: 60,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(4)),
+          child: TabBar(
+              controller: tabController,
+              indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color:
+                      Theme.of(context).colorScheme.onSurface.withOpacity(0.1)),
+              tabs: const [
+                Tab(
+                    icon: Icon(YaruIcons.input_keyboard),
+                    child: Text("Keyboard Settings")),
+                Tab(
+                    icon: Icon(YaruIcons.font),
+                    child: Text("Keyboard Shortcuts")),
+              ]),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: SizedBox(
+            height: 1000,
+            child: TabBarView(
+              controller: tabController,
+              children: [
+                createKeyBoardSettingsView(service, context),
+                const KeyboardShortcutsPage()
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column createKeyBoardSettingsView(
+      SettingsService service, BuildContext context) {
     return Column(
       children: [
         const YaruSection(headline: 'Input Sources', children: []),
@@ -25,22 +79,22 @@ class KeyboardPage extends StatelessWidget {
           child: const InputSourceSection(),
         ),
         const YaruSection(headline: 'Special characters', children: []),
-        YaruSection(headline: 'Keyboard Shortcuts', children: [
-          InkWell(
-            borderRadius: BorderRadius.circular(4),
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => const AlertDialog(
-                title: Text('Change Keyboard shortcuts'),
-                content: KeyboardShortcutsPage(),
-                actions: [],
-              ),
-            ),
-            child: const YaruRow(
-                trailingWidget: Text('Change keyboard shortcuts'),
-                actionWidget: Icon(YaruIcons.pan_end)),
-          )
-        ]),
+        // YaruSection(headline: 'Keyboard Shortcuts', children: [
+        //   InkWell(
+        //     borderRadius: BorderRadius.circular(4),
+        //     onTap: () => showDialog(
+        //       context: context,
+        //       builder: (context) => const AlertDialog(
+        //         title: Text('Change Keyboard shortcuts'),
+        //         content: KeyboardShortcutsPage(),
+        //         actions: [],
+        //       ),
+        //     ),
+        //     child: const YaruRow(
+        //         trailingWidget: Text('Change keyboard shortcuts'),
+        //         actionWidget: Icon(YaruIcons.pan_end)),
+        //   )
+        // ]),
       ],
     );
   }
