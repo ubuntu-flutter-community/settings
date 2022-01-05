ff07084224ce2ab8f8131ca30c8d86a645e10358
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-01-05 23:54:44 +0100

    update icon import

--- lib/view/pages/printers/printers_page.dart
+++ lib/view/pages/printers/printers_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:settings/view/pages/printers/printers_model.dart';
-import 'package:yaru_icons/widgets/yaru_icons.dart';
+import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 class PrintersPage extends StatelessWidget {
