75effb3e41e708ce573729ef2acbf49432a976c7
Michał <mivoligo@gmail.com>
2022-01-08 18:25:28 +0000

    Make getters in Mouse & Touchpad model nullable (#236)
    * Make getters nullable for general section
    
    * Make getters nullable for mouse section
    
    * Make getters nullable for touchpad section

--- lib/view/pages/mouse_and_touchpad/general_section.dart
+++ lib/view/pages/mouse_and_touchpad/general_section.dart
@@ -19,7 +19,9 @@ class GeneralSection extends StatelessWidget {
           labels: const ['Left', 'Right'],
           actionDescription:
               'Sets the order of physical buttons on mice and touchpads',
-          selectedValues: [!model.leftHanded, model.leftHanded],
+          selectedValues: model.leftHanded != null
+              ? [!model.leftHanded!, model.leftHanded!]
+              : null,
           onPressed: (index) => model.setLeftHanded(index == 1),
         ),
       ],
