5a5df7c8375bdbe141cded504636c77a920e9321
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-01-31 16:41:44 +0100

    Power: Add lid close section and model

---
+++ lib/view/pages/power/lid_close_model.dart
@@ -0,0 +1,43 @@
+import 'package:safe_change_notifier/safe_change_notifier.dart';
+import 'package:settings/schemas/schemas.dart';
+import 'package:settings/services/settings_service.dart';
+import 'package:settings/view/pages/power/lid_close_action.dart';
+
+const _lidCloseBatteryActionKey = 'lid-close-battery-action';
+const _lidCloseAcActionKey = 'lid-close-ac-action';
+
+class LidCloseModel extends SafeChangeNotifier {
+  final Settings? _daemonSettings;
+
+  LidCloseModel(SettingsService settings)
+      : _daemonSettings = settings.lookup(schemaSettingsDaemonPowerPlugin) {
+    _daemonSettings?.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    _daemonSettings?.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+  LidCloseAction? get acLidCloseAction =>
+      _daemonSettings?.stringValue(_lidCloseAcActionKey)?.toLidCloseAction();
+
+  set acLidCloseAction(LidCloseAction? value) {
+    if (value != null) {
+      _daemonSettings?.setValue(_lidCloseAcActionKey, value.name);
+      notifyListeners();
+    }
+  }
+
+  LidCloseAction? get batteryLidCloseAction => _daemonSettings
+      ?.stringValue(_lidCloseBatteryActionKey)
+      ?.toLidCloseAction();
+
+  set batteryLidCloseAction(LidCloseAction? value) {
+    if (value != null) {
+      _daemonSettings?.setValue(_lidCloseBatteryActionKey, value.name);
+      notifyListeners();
+    }
+  }
+}
