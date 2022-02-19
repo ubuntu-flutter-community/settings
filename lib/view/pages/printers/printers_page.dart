a12b1122cd17e97c371662af6b97870b31ff4a3f
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-02-19 21:03:48 +0100

    adapt to master

--- lib/view/pages/printers/printers_page.dart
+++ lib/view/pages/printers/printers_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:settings/constants.dart';
 import 'package:settings/l10n/l10n.dart';
 import 'package:settings/view/pages/printers/printers_model.dart';
 import 'package:yaru_icons/yaru_icons.dart';
@@ -28,110 +29,108 @@ class PrintersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return YaruPage(
       children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 35, top: 15),
-          child: SizedBox(
-            width: 518,
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 40,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        showDialog(
-                          barrierDismissible: false,
-                          context: context,
-                          builder: (_) =>
-                              StatefulBuilder(builder: (context, setState) {
-                            return const AddPrinterDialog();
-                          }),
-                        ).then((shortcut) {});
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: const [
-                          Icon(YaruIcons.plus),
-                          Padding(
-                            padding: EdgeInsets.only(left: 12.0, right: 2),
-                            child: Text('Add a printer'),
-                          )
-                        ],
-                      )),
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                SizedBox(
-                  height: 40,
-                  child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('Additional printer settings ...')),
-                )
-              ],
-            ),
+        SizedBox(
+          width: kDefaultWidth,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      barrierDismissible: false,
+                      context: context,
+                      builder: (_) =>
+                          StatefulBuilder(builder: (context, setState) {
+                        return const AddPrinterDialog();
+                      }),
+                    ).then((shortcut) {});
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Icon(YaruIcons.plus),
+                      Padding(
+                        padding: EdgeInsets.only(left: 12.0, right: 2),
+                        child: Text('Add a printer'),
+                      )
+                    ],
+                  )),
+              const SizedBox(
+                width: 12,
+              ),
+              OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('Additional printer settings ...'))
+            ],
           ),
         ),
+        const SizedBox(
+          height: 20,
+        ),
         for (var i = 0; i < 10; i++)
-          YaruSection(headline: 'Printer ${i + 1}', children: [
-            YaruRow(
-              trailingWidget: Row(
-                children: [
-                  SizedBox(
-                    width: 70,
-                    child: Image.asset(
-                        i.isEven
-                            ? 'assets/images/icons/printer.png'
-                            : 'assets/images/icons/printer-network.png',
-                        fit: BoxFit.fill),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  SizedBox(
-                    height: 60,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Model XYZ'), // printer.type
-                        const SizedBox(
-                          height: 3,
+          YaruSection(
+              width: kDefaultWidth,
+              headline: 'Printer ${i + 1}',
+              children: [
+                YaruRow(
+                  trailingWidget: Row(
+                    children: [
+                      SizedBox(
+                        width: 70,
+                        child: Image.asset(
+                            i.isEven
+                                ? 'assets/images/icons/printer.png'
+                                : 'assets/images/icons/printer-network.png',
+                            fit: BoxFit.fill),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      SizedBox(
+                        height: 60,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Model XYZ'), // printer.type
+                            const SizedBox(
+                              height: 3,
+                            ),
+                            Text(i.isEven
+                                ? 'local'
+                                : '192.168.1.1337'), // printer.location
+                            // printer.status
+                          ],
                         ),
-                        Text(i.isEven
-                            ? 'local'
-                            : '192.168.1.1337'), // printer.location
-                        // printer.status
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              actionWidget: Row(
-                children: [
-                  const SizedBox(
-                    height: 40,
-                    child: OutlinedButton(
-                        onPressed: null, // printer.activejobs ?
-                        child: Text(
-                          'No active jobs',
-                          // style: TextStyle(
-                          //     color: Theme.of(context).disabledColor),
-                        )),
+                      )
+                    ],
                   ),
-                  const SizedBox(
-                    width: 10,
+                  actionWidget: Row(
+                    children: [
+                      const SizedBox(
+                        height: 40,
+                        child: OutlinedButton(
+                            onPressed: null, // printer.activejobs ?
+                            child: Text(
+                              'No active jobs',
+                              // style: TextStyle(
+                              //     color: Theme.of(context).disabledColor),
+                            )),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      YaruOptionButton(
+                        iconData: YaruIcons.settings,
+                        onPressed: () => {},
+                      ),
+                    ],
                   ),
-                  YaruOptionButton(
-                    iconData: YaruIcons.settings,
-                    onPressed: () => {},
-                  ),
-                ],
-              ),
-              enabled: true,
-            )
-          ])
+                  enabled: true,
+                )
+              ])
       ],
     );
   }
