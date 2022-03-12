2b93129f7ec799bafd4d3ffd5a27698551c70b2b
tarek <atsen.dev@gmail.com>
2022-03-12 18:24:55 +0100

    fix bad expand on sections (due to ListView)

--- lib/view/pages/displays/displays_page.dart
+++ lib/view/pages/displays/displays_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:settings/constants.dart';
 import 'package:settings/l10n/l10n.dart';
 import 'package:settings/services/display/display_service.dart';
 import 'package:settings/view/pages/displays/displays_configuration.dart';
 import 'package:settings/view/pages/displays/displays_model.dart';
-import 'package:settings/view/pages/displays/enums/displays_page_section.dart';
 import 'package:settings/view/pages/displays/widgets/monitor_section.dart';
+import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 class DisplaysPage extends StatefulWidget {
@@ -28,96 +29,112 @@ class DisplaysPage extends StatefulWidget {
 }
 
 class _DisplaysPageState extends State<DisplaysPage> {
-  late DisplaysPageSection _selectedSection;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _selectedSection = DisplaysPageSection.displays;
-  }
-
-  void _switchToSection(DisplaysPageSection section) {
-    setState(() {
-      _selectedSection = section;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final model = context.watch<DisplaysModel>();
 
     return ValueListenableBuilder<DisplaysConfiguration?>(
         valueListenable: model.configuration,
-        builder: (BuildContext context, DisplaysConfiguration? configurations, _) {
-          return YaruPage(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Builder(
-                    builder: (context) {
-                      Widget buttons;
-                      if (model.modifyMode) {
-                        buttons = Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            OutlinedButton(
-                                onPressed: model.apply,
-                                child: Text(context.l10n.apply)),
-                          ],
-                        );
-                      } else {
-                        buttons = YaruToggleButtonsRow(
-                          actionLabel: '',
-
-                          /// top right buttons labels
-                          labels: DisplaysPageSection.values
-                              .map((e) => e.name(context))
-                              .toList(),
-                          selectedValues: DisplaysPageSection.values
-                              .map((e) => e == _selectedSection)
-                              .toList(),
-
-                          /// when press on a button, change view by changing
-                          /// selected value
-                          onPressed: (index) => _switchToSection(
-                              DisplaysPageSection.values[index]),
-                        );
-                      }
-
-                      return SizedBox(
-                        height: 60,
-                        child: buttons,
-                      );
-                    },
-                  ),
-                  const Divider(
-                    color: Colors.transparent,
-                    height: 4,
-                  ),
-                  Builder(
-                    builder: (context) {
-                      switch (_selectedSection) {
-                        case DisplaysPageSection.displays:
-                          return ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: configurations?.configurations.length ?? 0,
-                            itemBuilder: (context, index) => MonitorSection(
-                              index: index,
-                            ),
-                          );
-                        case DisplaysPageSection.night:
-                          return Center(
-                            child: Text(context.l10n.nightMode),
-                          );
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ],
+        builder:
+            (BuildContext context, DisplaysConfiguration? configurations, _) {
+          return YaruTabbedPage(
+            width: kDefaultWidth,
+            tabIcons:
+                DisplaysPageSection.values.map((e) => e.icon(context)).toList(),
+            tabTitles:
+                DisplaysPageSection.values.map((e) => e.name(context)).toList(),
+            views: DisplaysPageSection.values
+                .map((e) => _buildPage(e, model, configurations))
+                .toList(),
           );
         });
+  }
+
+  Widget _buildPage(
+    DisplaysPageSection section,
+    DisplaysModel model,
+    DisplaysConfiguration? configurations,
+  ) {
+    switch (section) {
+      case DisplaysPageSection.displays:
+        return YaruPage(
+          children: [
+            SizedBox(
+              width: kDefaultWidth,
+              child: Visibility(
+                visible: model.modifyMode,
+                maintainSize: true,
+                maintainAnimation: true,
+                maintainState: true,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        OutlinedButton(
+                          onPressed: model.apply,
+                          child: Text(context.l10n.apply),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.transparent,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: kDefaultWidth,
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: configurations?.configurations.length ?? 0,
+                itemBuilder: (context, index) => MonitorSection(
+                  index: index,
+                ),
+              ),
+            ),
+          ],
+        );
+      case DisplaysPageSection.night:
+        return YaruPage(
+          children: [
+            Center(
+              child: Text(context.l10n.nightMode),
+            )
+          ],
+        );
+      default:
+        return Container();
+    }
+  }
+}
+
+enum DisplaysPageSection {
+  displays,
+  night,
+}
+
+extension DisplaysPageSectionExtension on DisplaysPageSection {
+  String name(BuildContext context) {
+    switch (this) {
+      case DisplaysPageSection.displays:
+        return context.l10n.displays;
+      case DisplaysPageSection.night:
+        return context.l10n.nightMode;
+      default:
+        return '';
+    }
+  }
+
+  IconData icon(BuildContext context) {
+    switch (this) {
+      case DisplaysPageSection.displays:
+        return YaruIcons.desktop_display;
+      case DisplaysPageSection.night:
+        return YaruIcons.weather_clear_night;
+      default:
+        return Icons.check_box_outline_blank;
+    }
   }
 }
--- lib/view/pages/displays/widgets/monitor_section.dart
+++ lib/view/pages/displays/widgets/monitor_section.dart
@@ -22,101 +22,104 @@ class MonitorSection extends StatelessWidget {
       valueListenable: model.configuration,
       builder: (context, value, child) {
         final DisplayMonitorConfiguration config = value!.configurations[index];
-        return YaruSection(
-          headline: config.name,
-          width: kDefaultWidth,
-          children: <Widget>[
-            /// Orientation row
-            YaruRow(
-              enabled: true,
-              trailingWidget: Container(),
-              leadingWidget: Text(context.l10n.orientation),
-              actionWidget: DropdownButton<LogicalMonitorOrientation>(
-                value: config.transform!,
-                items: [
-                  for (final LogicalMonitorOrientation value
-                      in model.displayableOrientations)
-                    DropdownMenuItem(
-                      value: value,
-                      child: Text(value.translate(context)),
-                    ),
-                ],
-                onChanged: (value) {
-                  model.setOrientation(index, value!);
-                },
+        return ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: kDefaultWidth),
+          child: YaruSection(
+            headline: config.name,
+            //width: kDefaultWidth,
+            children: <Widget>[
+              /// Orientation row
+              YaruRow(
+                enabled: true,
+                trailingWidget: Container(),
+                leadingWidget: Text(context.l10n.orientation),
+                actionWidget: DropdownButton<LogicalMonitorOrientation>(
+                  value: config.transform!,
+                  items: [
+                    for (final LogicalMonitorOrientation value
+                        in model.displayableOrientations)
+                      DropdownMenuItem(
+                        value: value,
+                        child: Text(value.translate(context)),
+                      ),
+                  ],
+                  onChanged: (value) {
+                    model.setOrientation(index, value!);
+                  },
+                ),
               ),
-            ),
 
-            /// Resolution row
-            YaruRow(
-              enabled: true,
-              trailingWidget: Container(),
-              leadingWidget: Text(context.l10n.resolution),
-              actionWidget: DropdownButton<String>(
-                value: config.resolution,
-                items: [
-                  for (final String value in config.availableResolutions)
-                    DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    ),
-                ],
-                onChanged: (value) => model.setResolution(index, value!),
+              /// Resolution row
+              YaruRow(
+                enabled: true,
+                trailingWidget: Container(),
+                leadingWidget: Text(context.l10n.resolution),
+                actionWidget: DropdownButton<String>(
+                  value: config.resolution,
+                  items: [
+                    for (final String value in config.availableResolutions)
+                      DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      ),
+                  ],
+                  onChanged: (value) => model.setResolution(index, value!),
+                ),
               ),
-            ),
 
-            /// Refresh rate row
-            YaruRow(
-              enabled: true,
-              trailingWidget: Container(),
-              leadingWidget: Text(context.l10n.refreshRate),
-              actionWidget: DropdownButton<String>(
-                value: config.refreshRate,
-                items: [
-                  for (final value in config.availableRefreshRates)
-                    DropdownMenuItem(
-                      value: value,
-                      child: Text(
-                          double.parse(value).toStringAsFixed(2).toString()),
-                    ),
-                ],
-                onChanged: (String? value) =>
-                    model.setRefreshRate(index, value!),
+              /// Refresh rate row
+              YaruRow(
+                enabled: true,
+                trailingWidget: Container(),
+                leadingWidget: Text(context.l10n.refreshRate),
+                actionWidget: DropdownButton<String>(
+                  value: config.refreshRate,
+                  items: [
+                    for (final value in config.availableRefreshRates)
+                      DropdownMenuItem(
+                        value: value,
+                        child: Text(
+                            double.parse(value).toStringAsFixed(2).toString()),
+                      ),
+                  ],
+                  onChanged: (String? value) =>
+                      model.setRefreshRate(index, value!),
+                ),
               ),
-            ),
 
-            /// Scale row
-            YaruRow(
-              enabled: true,
-              leadingWidget: Text(context.l10n.scale),
-              actionWidget: const SizedBox(),
-              trailingWidget: YaruToggleButtonsRow(
-                actionLabel: '',
-                labels: config.availableScales
-                    .map((scale) => scale.toInt() * 100)
-                    .map(context.l10n.scaleFormat)
-                    .toList(),
-                selectedValues: config.availableScales
-                    .map((double scale) => scale == config.scale)
-                    .toList(),
-                onPressed: (int scaleIndex) {
-                  model.setScale(
-                    index,
-                    config.availableScales[scaleIndex],
-                  );
-                },
+              /// Scale row
+              YaruRow(
+                enabled: true,
+                leadingWidget: Text(context.l10n.scale),
+                actionWidget: const SizedBox(),
+                trailingWidget: YaruToggleButtonsRow(
+                  actionLabel: '',
+                  labels: config.availableScales
+                      .map((scale) => scale.toInt() * 100)
+                      .map(context.l10n.scaleFormat)
+                      .toList(),
+                  selectedValues: config.availableScales
+                      .map((double scale) => scale == config.scale)
+                      .toList(),
+                  onPressed: (int scaleIndex) {
+                    model.setScale(
+                      index,
+                      config.availableScales[scaleIndex],
+                    );
+                  },
+                ),
               ),
-            ),
 
-            /// Fractional Scaling row
-            YaruSwitchRow(
-              enabled: false,
-              trailingWidget: Text(context.l10n.fractionalScaling),
-              actionDescription: context.l10n.fractionalScaling_description,
-              onChanged: (bool value) {},
-              value: null,
-            ),
-          ],
+              /// Fractional Scaling row
+              YaruSwitchRow(
+                enabled: false,
+                trailingWidget: Text(context.l10n.fractionalScaling),
+                actionDescription: context.l10n.fractionalScaling_description,
+                onChanged: (bool value) {},
+                value: null,
+              ),
+            ],
+          ),
         );
       },
     );
