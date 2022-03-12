8136f5fa33b0e8c7db84b07000d8fda5aede5e9b
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-03-12 22:22:59 +0100

    BluetoothDeviceModel: fix warning from #295 plus..
    ..added generated_plugins.cmake, doesn't hurt

--- lib/view/pages/bluetooth/bluetooth_device_model.dart
+++ lib/view/pages/bluetooth/bluetooth_device_model.dart
@@ -85,7 +85,7 @@ class BluetoothDeviceModel extends SafeChangeNotifier {
     blocked = _device.blocked;
     address = _device.address;
     paired = _device.paired;
-    adapter = _device.adapter!;//TODO forced to put non nullable
+    adapter = _device.adapter;
     addressType = _device.addressType;
     icon = _device.icon;
     legacyPairing = _device.legacyPairing;
