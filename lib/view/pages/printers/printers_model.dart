8c813ccd41e473355305a30672b410d5249d77a3
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-03-13 23:46:08 +0100

    First real printer tests

--- lib/view/pages/printers/printers_model.dart
+++ lib/view/pages/printers/printers_model.dart
@@ -1,3 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:safe_change_notifier/safe_change_notifier.dart';
 
-class PrintersModel extends SafeChangeNotifier {}
+class PrintersModel extends SafeChangeNotifier {
+  void openPrinterSettings() {
+    Process.run('system-config-printer', ['&']);
+  }
+
+  Future<List<String>> loadPrinters() async {
+    final List<String> printerNames = [];
+    await Process.run('lpinfo', ['-v']).then((value) {
+      printerNames.addAll(const LineSplitter().convert(value.stdout));
+      printerNames.retainWhere((element) =>
+          element.contains('network dnssd://') ||
+          element.contains('network ipps://'));
+    });
+    return printerNames;
+  }
+}
--- lib/view/pages/printers/printers_page.dart
+++ lib/view/pages/printers/printers_page.dart
@@ -6,7 +6,7 @@ import 'package:settings/view/pages/printers/printers_model.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
-class PrintersPage extends StatelessWidget {
+class PrintersPage extends StatefulWidget {
   const PrintersPage({Key? key}) : super(key: key);
 
   static Widget create(BuildContext context) {
@@ -28,134 +28,175 @@ class PrintersPage extends StatelessWidget {
           : false;
 
   @override
+  State<PrintersPage> createState() => _PrintersPageState();
+}
+
+class _PrintersPageState extends State<PrintersPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final model = context.watch<PrintersModel>();
     return YaruPage(
       children: [
-        SizedBox(
-          width: kDefaultWidth,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                      barrierDismissible: false,
-                      context: context,
-                      builder: (_) =>
-                          StatefulBuilder(builder: (context, setState) {
-                        return AddPrinterDialog(
-                          children: [
-                            Icon(
-                              YaruIcons.printer,
-                              color: Theme.of(context).colorScheme.primary,
-                              size: 50,
-                            )
-                          ],
-                          onConfirm: () {
-                            Navigator.of(context).pop();
-                            return ScaffoldMessenger.of(context)
-                                .showSnackBar(SnackBar(
-                                    content: Icon(
-                              YaruIcons.printer,
-                              color: Theme.of(context).colorScheme.primary,
-                            )));
-                          },
-                          title: 'Add Printer',
-                        );
-                      }),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      Icon(YaruIcons.plus),
-                      Padding(
-                        padding: EdgeInsets.only(left: 12.0, right: 2),
-                        child: Text('Add a printer'),
-                      )
-                    ],
-                  )),
-              const SizedBox(
-                width: 12,
-              ),
-              YaruOptionButton(onPressed: () {}, iconData: YaruIcons.settings)
-            ],
-          ),
+        ChangeNotifierProvider.value(
+          value: model,
+          child: const _Header(),
         ),
         const SizedBox(
           height: 20,
         ),
-        for (var i = 0; i < 10; i++)
-          YaruSection(
-              width: kDefaultWidth,
-              headline: 'Printer ${i + 1}',
-              children: [
-                YaruRow(
-                  trailingWidget: Row(
-                    children: [
-                      SizedBox(
-                        width: 70,
-                        child: Image.asset(
-                            i.isEven
-                                ? 'assets/images/icons/printer.png'
-                                : 'assets/images/icons/printer-network.png',
-                            fit: BoxFit.fill),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      Expanded(
-                        child: SizedBox(
-                          height: 60,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Flexible(
-                                  child: Text(
-                                'Model XYZ',
-                                overflow: TextOverflow.ellipsis,
-                              )), // printer.type
-                              const SizedBox(
-                                height: 3,
-                              ),
-                              Flexible(
-                                child: Text(
-                                    i.isEven ? 'local' : '192.168.1.1337',
-                                    overflow: TextOverflow.ellipsis),
-                              ), // printer.location
-                              // printer.status
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  actionWidget: Row(
-                    children: [
-                      const SizedBox(
-                        height: 40,
-                        child: OutlinedButton(
-                            onPressed: null, // printer.activejobs ?
-                            child: Text(
-                              'No active jobs',
-                              // style: TextStyle(
-                              //     color: Theme.of(context).disabledColor),
-                            )),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      YaruOptionButton(
-                        iconData: YaruIcons.settings,
-                        onPressed: () => {},
-                      ),
-                    ],
-                  ),
-                  enabled: true,
-                )
-              ])
+        FutureBuilder<List<String>>(
+            future: model.loadPrinters(),
+            builder: (context, snapshot) => snapshot.hasData
+                ? ListView(
+                    shrinkWrap: true,
+                    children: snapshot.data!
+                        .map((e) => _PrinterSection(name: e))
+                        .toList(),
+                  )
+                : const LinearProgressIndicator())
       ],
+    );
+  }
+}
+
+class _PrinterSection extends StatelessWidget {
+  const _PrinterSection({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return YaruSection(width: kDefaultWidth, headline: name, children: [
+      YaruRow(
+        trailingWidget: Row(
+          children: [
+            SizedBox(
+              width: 70,
+              child: Image.asset('assets/images/icons/printer.png',
+                  fit: BoxFit.fill),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: SizedBox(
+                height: 60,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Flexible(
+                        child: Text(
+                      'Model XYZ',
+                      overflow: TextOverflow.ellipsis,
+                    )), // printer.type
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Flexible(
+                      child: Text('bla', overflow: TextOverflow.ellipsis),
+                    ), // printer.location
+                    // printer.status
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+        actionWidget: Row(
+          children: [
+            const SizedBox(
+              height: 40,
+              child: OutlinedButton(
+                  onPressed: null, // printer.activejobs ?
+                  child: Text(
+                    'No active jobs',
+                    // style: TextStyle(
+                    //     color: Theme.of(context).disabledColor),
+                  )),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            YaruOptionButton(
+              iconData: YaruIcons.settings,
+              onPressed: () => {},
+            ),
+          ],
+        ),
+        enabled: true,
+      )
+    ]);
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.watch<PrintersModel>();
+    return SizedBox(
+      width: kDefaultWidth,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (_) => StatefulBuilder(builder: (context, setState) {
+                    return AddPrinterDialog(
+                      children: [
+                        Icon(
+                          YaruIcons.printer,
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 50,
+                        )
+                      ],
+                      onConfirm: () {
+                        Navigator.of(context).pop();
+                        return ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(
+                                content: Icon(
+                          YaruIcons.printer,
+                          color: Theme.of(context).colorScheme.primary,
+                        )));
+                      },
+                      title: 'Add Printer',
+                    );
+                  }),
+                );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(YaruIcons.plus),
+                  Padding(
+                    padding: EdgeInsets.only(left: 12.0, right: 2),
+                    child: Text('Add a printer'),
+                  )
+                ],
+              )),
+          const SizedBox(
+            width: 12,
+          ),
+          YaruOptionButton(
+              onPressed: () => model.openPrinterSettings(),
+              iconData: YaruIcons.settings)
+        ],
+      ),
     );
   }
 }
