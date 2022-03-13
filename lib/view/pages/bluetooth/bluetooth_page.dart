ubuntu-flutter-community/settings

commit 835544eb2641ed6eb815370938d0ecc2c19aa67e

Add Display Page (without Night Mode & fract. scaling) (#295)
* Adding Display Page (without Night Mode), WIP on multiple screen configuration

* fix issue when no current option in a monitor

* fix bad expand on sections (due to ListView)

* changing scale format / changing apply button to an ElevatedButton

lib/view/pages/bluetooth/bluetooth_page.dart
@@ -40,6 +40,13 @@ class _BluetoothPageState extends State<BluetoothPage> {
   }
 
   @override
+  void dispose() {
+    final model = context.read<BluetoothModel>();
+    model.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final model = context.watch<BluetoothModel>();
     return YaruPage(

analysis_options.yaml
@@ -15,6 +15,7 @@ analyzer:
     - "**/*.g.dart"
     - "**/*.mocks.dart"
     - "**/l10/*.dart"
+    - "**/generated/**"
 
 linter:
   # The lint rules applied to this project can be customized in the

lib/generated/dbus/display-config-remote-object.dart
@@ -1,0 +1,85 @@
+// This file was generated using the following command and may be overwritten.
+// dart-dbus generate-remote-object interfaces/org.gnome.Mutter.DisplayConfig.xml
+
+import 'package:dbus/dbus.dart';
+
+/// Signal data for org.gnome.Mutter.DisplayConfig.MonitorsChanged.
+class OrgGnomeMutterDisplayConfigMonitorsChanged extends DBusSignal {
+  OrgGnomeMutterDisplayConfigMonitorsChanged(DBusSignal signal) : super(sender: signal.sender, path: signal.path, interface: signal.interface, name: signal.name, values: signal.values);
+}
+
+class OrgGnomeMutterDisplayConfig extends DBusRemoteObject {
+  /// Stream of org.gnome.Mutter.DisplayConfig.MonitorsChanged signals.
+  late final Stream<OrgGnomeMutterDisplayConfigMonitorsChanged> monitorsChanged;
+
+  OrgGnomeMutterDisplayConfig(DBusClient client, String destination, DBusObjectPath path) : super(client, name: destination, path: path) {
+    monitorsChanged = DBusRemoteObjectSignalStream(object: this, interface: 'org.gnome.Mutter.DisplayConfig', name: 'MonitorsChanged', signature: DBusSignature('')).asBroadcastStream().map((signal) => OrgGnomeMutterDisplayConfigMonitorsChanged(signal));
+  }
+
+  /// Gets org.gnome.Mutter.DisplayConfig.PowerSaveMode
+  Future<int> getPowerSaveMode() async {
+    var value = await getProperty('org.gnome.Mutter.DisplayConfig', 'PowerSaveMode', signature: DBusSignature('i'));
+    return (value as DBusInt32).value;
+  }
+
+  /// Sets org.gnome.Mutter.DisplayConfig.PowerSaveMode
+  Future<void> setPowerSaveMode (int value) async {
+    await setProperty('org.gnome.Mutter.DisplayConfig', 'PowerSaveMode', DBusInt32(value));
+  }
+
+  /// Gets org.gnome.Mutter.DisplayConfig.PanelOrientationManaged
+  Future<bool> getPanelOrientationManaged() async {
+    var value = await getProperty('org.gnome.Mutter.DisplayConfig', 'PanelOrientationManaged', signature: DBusSignature('b'));
+    return (value as DBusBoolean).value;
+  }
+
+  /// Gets org.gnome.Mutter.DisplayConfig.ApplyMonitorsConfigAllowed
+  Future<bool> getApplyMonitorsConfigAllowed() async {
+    var value = await getProperty('org.gnome.Mutter.DisplayConfig', 'ApplyMonitorsConfigAllowed', signature: DBusSignature('b'));
+    return (value as DBusBoolean).value;
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.GetResources()
+  Future<List<DBusValue>> callGetResources({bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    var result = await callMethod('org.gnome.Mutter.DisplayConfig', 'GetResources', [], replySignature: DBusSignature('ua(uxiiiiiuaua{sv})a(uxiausauaua{sv})a(uxuudu)ii'), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+    return result.returnValues;
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.ApplyConfiguration()
+  Future<void> callApplyConfiguration(int serial, bool persistent, List<DBusStruct> crtcs, List<DBusStruct> outputs, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    await callMethod('org.gnome.Mutter.DisplayConfig', 'ApplyConfiguration', [DBusUint32(serial), DBusBoolean(persistent), DBusArray(DBusSignature('(uiiiuaua{sv})'), crtcs.map((child) => child)), DBusArray(DBusSignature('(ua{sv})'), outputs.map((child) => child))], replySignature: DBusSignature(''), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.ChangeBacklight()
+  Future<int> callChangeBacklight(int serial, int output, int value, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    var result = await callMethod('org.gnome.Mutter.DisplayConfig', 'ChangeBacklight', [DBusUint32(serial), DBusUint32(output), DBusInt32(value)], replySignature: DBusSignature('i'), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+    return (result.returnValues[0] as DBusInt32).value;
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.GetCrtcGamma()
+  Future<List<DBusValue>> callGetCrtcGamma(int serial, int crtc, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    var result = await callMethod('org.gnome.Mutter.DisplayConfig', 'GetCrtcGamma', [DBusUint32(serial), DBusUint32(crtc)], replySignature: DBusSignature('aqaqaq'), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+    return result.returnValues;
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.SetCrtcGamma()
+  Future<void> callSetCrtcGamma(int serial, int crtc, List<int> red, List<int> green, List<int> blue, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    await callMethod('org.gnome.Mutter.DisplayConfig', 'SetCrtcGamma', [DBusUint32(serial), DBusUint32(crtc), DBusArray.uint16(red), DBusArray.uint16(green), DBusArray.uint16(blue)], replySignature: DBusSignature(''), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.GetCurrentState()
+  Future<List<DBusValue>> callGetCurrentState({bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    var result = await callMethod('org.gnome.Mutter.DisplayConfig', 'GetCurrentState', [], replySignature: DBusSignature('ua((ssss)a(siiddada{sv})a{sv})a(iiduba(ssss)a{sv})a{sv}'), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+    return result.returnValues;
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.ApplyMonitorsConfig()
+  Future<void> callApplyMonitorsConfig(int serial, int method, List<DBusStruct> logical_monitors, Map<String, DBusValue> properties, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    await callMethod('org.gnome.Mutter.DisplayConfig', 'ApplyMonitorsConfig', [DBusUint32(serial), DBusUint32(method), DBusArray(DBusSignature('(iiduba(ssa{sv}))'), logical_monitors.map((child) => child)), DBusDict.stringVariant(properties)], replySignature: DBusSignature(''), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+  }
+
+  /// Invokes org.gnome.Mutter.DisplayConfig.SetOutputCTM()
+  Future<void> callSetOutputCTM(int serial, int output, DBusStruct ctm, {bool noAutoStart = false, bool allowInteractiveAuthorization = false}) async {
+    await callMethod('org.gnome.Mutter.DisplayConfig', 'SetOutputCTM', [DBusUint32(serial), DBusUint32(output), ctm], replySignature: DBusSignature(''), noAutoStart: noAutoStart, allowInteractiveAuthorization: allowInteractiveAuthorization);
+  }
+}

lib/l10n/app_en.arb
@@ -129,5 +129,25 @@
   "dateAndTimePageSecondsInPanel": "Show seconds in panel",
   "dateAndTimePageWeekdayInPanel": "Show weekday in panel",
   "dateAndTimePageWeekNumberInCalendar": "Show week number in calendar",
-  "infoPageTitle": "Info"
+  "infoPageTitle": "Info",
+  "apply" : "Apply",
+  "displays": "Displays",
+  "nightMode": "Night Mode",
+  "orientation": "Orientation",
+  "resolution": "Resolution",
+  "refreshRate": "Refresh Rate",
+  "scale" : "Scale",
+  "fractionalScaling" : "Fractional Scaling",
+  "fractionalScaling_description" : "May increase power usage, lower speed, or reduce display sharpness",
+  "landscape" : "Landscape",
+  "portraitLeft" : "Portrait on the left",
+  "portraitRight" : "Portrait on the right",
+  "flippedLandscape" : "Flipped landscape",
+
+  "scaleFormat": "{factor}x",
+  "@scaleFormat" : {
+    "placeholders": {
+      "factor": {}
+    }
+  }
 }

lib/l10n/app_fr.arb
@@ -107,5 +107,17 @@
   "usersPageTitle": "Utilisateurs",
   "defaultAppsPageTitle": "Applications par défaut",
   "dateAndTimePageTitle": "Date et heure",
-  "infoPageTitle": "À propos"
+  "infoPageTitle": "À propos",
+  "apply" : "Appliquer",
+  "displays": "Écran",
+  "nightMode": "Mode Nuit",
+  "orientation": "Orientation",
+  "resolution": "Résolution",
+  "refreshRate": "Fréquence de rafraîchissement",
+  "scale" : "Échelle",
+  "fractionalScaling_description" : "Peut augmenter votre consommation d'energie, réduire la vitesse ou réduire la netteté de l'affichage",
+  "landscape" : "Paysage",
+  "portraitLeft" : "Portrait sur la gauche",
+  "portraitRight" : "Portrait sur la droite",
+  "flippedLandscape" : "Paysage inversé"
 }

lib/main.dart
@@ -6,6 +6,7 @@ import 'package:settings/app.dart';
 import 'package:settings/schemas/schemas.dart';
 import 'package:settings/services/bluetooth_service.dart';
 import 'package:settings/services/date_time_service.dart';
+import 'package:settings/services/display/display_service.dart';
 import 'package:settings/services/hostname_service.dart';
 import 'package:settings/services/house_keeping_service.dart';
 import 'package:settings/services/input_source_service.dart';
@@ -79,6 +80,11 @@ void main() async {
         Provider<LocaleService>(
           create: (_) => LocaleService(),
           dispose: (_, service) => service.dispose(),
+        ),
+        Provider<DisplayService>(
+          create: (_) => DisplayService(),
+          //TODO
+          //dispose: (_, DisplayService service) => service.dispose(),
         ),
       ],
       child: const UbuntuSettingsApp(),

lib/services/display/display_dbus_service.dart
@@ -1,0 +1,84 @@
+import 'package:dbus/dbus.dart';
+import 'package:settings/services/display/objects/dbus_displays_config.dart';
+import 'package:settings/generated/dbus/display-config-remote-object.dart';
+
+const _displaysInterface = 'org.gnome.Mutter.DisplayConfig';
+
+const _displayPath = '/org/gnome/Mutter/DisplayConfig';
+
+///
+/// Methods:
+///
+/// ApplyConfiguration (UInt32 serial, Boolean persistent, Array of [Struct of (UInt32, Int32, Int32, Int32, UInt32, Array of [UInt32], Dict of {String, Variant})] crtcs, Array of [Struct of (UInt32, Dict of {String, Variant})] outputs) ↦ ()
+/// ApplyMonitorsConfig (UInt32 serial, UInt32 method, Array of [Struct of (Int32, Int32, Double, UInt32, Boolean, Array of [Struct of (String, String, Dict of {String, Variant})])] logical_monitors, Dict of {String, Variant} properties) ↦ ()
+/// ChangeBacklight (UInt32 serial, UInt32 output, Int32 value) ↦ (Int32 new_value)
+/// GetCrtcGamma (UInt32 serial, UInt32 crtc) ↦ (Array of [UInt16] red, Array of [UInt16] green, Array of [UInt16] blue)
+/// GetCurrentState () ↦ (UInt32 serial, Array of [Struct of (Struct of (String, String, String, String), Array of [Struct of (String, Int32, Int32, Double, Double, Array of [Double], Dict of {String, Variant})], Dict of {String, Variant})] monitors, Array of [Struct of (Int32, Int32, Double, UInt32, Boolean, Array of [Struct of (String, String, String, String)], Dict of {String, Variant})] logical_monitors, Dict of {String, Variant} properties)
+/// GetResources () ↦ (UInt32 serial, Array of [Struct of (UInt32, Int64, Int32, Int32, Int32, Int32, Int32, UInt32, Array of [UInt32], Dict of {String, Variant})] crtcs, Array of [Struct of (UInt32, Int64, Int32, Array of [UInt32], String, Array of [UInt32], Array of [UInt32], Dict of {String, Variant})] outputs, Array of [Struct of (UInt32, Int64, UInt32, UInt32, Double, UInt32)] modes, Int32 max_screen_width, Int32 max_screen_height)
+/// SetCrtcGamma (UInt32 serial, UInt32 crtc, Array of [UInt16] red, Array of [UInt16] green, Array of [UInt16] blue) ↦ ()
+/// SetOutputCTM (UInt32 serial, UInt32 output, Struct of (UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64) ctm) ↦ ()
+///
+/// Signals:
+///
+/// MonitorsChanged
+class DisplayDBusService {
+  DisplayDBusService() : _object = _createObject() {
+    /// Listen to signal stream, when a change occur, we update our data
+    _object.monitorsChanged
+        .listen((OrgGnomeMutterDisplayConfigMonitorsChanged signal) {
+      if (signal.name == 'MonitorsChanged') {
+        getCurrent();
+      }
+    });
+  }
+
+  final OrgGnomeMutterDisplayConfig _object;
+
+  Stream<DBusDisplaysConfig> get streamChanges =>
+      _object.monitorsChanged
+          .asyncMap((event) async => getCurrent());
+
+  static OrgGnomeMutterDisplayConfig _createObject() =>
+      OrgGnomeMutterDisplayConfig(
+        DBusClient.session(),
+        _displaysInterface,
+        DBusObjectPath(_displayPath),
+      );
+
+  Future<DBusDisplaysConfig> getCurrent() async {
+    List<DBusValue>? state = await _object.callGetCurrentState();
+    List<dynamic> list = state.map((e) => _toNative(e)).toList();
+    return DBusDisplaysConfig(list);
+  }
+
+  Future<void> apply(int serial, ConfigurationMethod configurationMethod,
+      List<DBusStruct> logicalParameterValues) =>
+      _object.callApplyMonitorsConfig(
+        serial,
+        configurationMethod.index,
+        logicalParameterValues,
+        {},
+      );
+
+  dynamic _toNative(dynamic value) {
+    dynamic output;
+
+    if (value is Map) {
+      output =
+          value.map((key, value) => MapEntry(_toNative(key), _toNative(value)));
+    } else if (value is Iterable) {
+      output = value.map((e) => _toNative(e)).toList();
+    } else if (value is DBusArray) {
+      output = value.toNative().map((e) => _toNative(e)).toList();
+    } else if (value is DBusValue) {
+      output = value.toNative();
+    } else {
+      return value;
+    }
+
+    return output;
+  }
+
+}
+
+enum ConfigurationMethod { verify, temporary, persistent }

lib/services/display/display_service.dart
@@ -1,0 +1,115 @@
+import 'dart:async';
+
+import 'package:dbus/dbus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:settings/services/display/display_dbus_service.dart';
+import 'package:settings/services/display/objects/dbus_displays_config.dart';
+import 'package:settings/view/pages/displays/displays_configuration.dart';
+
+class DisplayService {
+  DisplayService()
+      : _displayDBusService = DisplayDBusService(),
+        _currentNotifier = ValueNotifier(null),
+        _initialNotifier = ValueNotifier(null) {
+
+    _loadState(notifyStream: true).then((DisplaysConfiguration value) {
+      _initialNotifier.value = value;
+      _currentNotifier.value = value;
+    });
+
+    _displayDBusService.streamChanges
+        .map(_mapToModel)
+        .listen(_monitorStateStreamController.add);
+  }
+
+  final DisplayDBusService _displayDBusService;
+
+  final StreamController<DisplaysConfiguration> _monitorStateStreamController =
+      StreamController<DisplaysConfiguration>.broadcast();
+
+  Stream<DisplaysConfiguration> get monitorStateStream =>
+      _monitorStateStreamController.stream;
+
+  DisplaysConfiguration? latest;
+
+  /// NOTIFIERS
+  final ValueNotifier<DisplaysConfiguration?> _initialNotifier;
+  ValueNotifier<DisplaysConfiguration?> get initialNotifier => _initialNotifier;
+
+  final ValueNotifier<DisplaysConfiguration?> _currentNotifier;
+  ValueNotifier<DisplaysConfiguration?> get currentNotifier => _currentNotifier;
+
+  Future<void> applyConfig() async {
+    /// do nothing if no way to perform applyConfig (made to be safe, but we may
+    /// never pass here)
+    if(_currentNotifier.value == null){}
+
+    final DBusDisplaysConfig displayConfig =
+        await _displayDBusService.getCurrent();
+
+    List<DBusStruct> logicalParameterValues = [];
+
+    for (int i = 0; i < displayConfig.monitorsLength; i++) {
+      final DisplayMonitorConfiguration confMonitor =
+      _currentNotifier.value!.configurations[i];
+
+      // x ; y ; scale ; transform(rotation) ; primary ; monitors
+      logicalParameterValues.add(DBusStruct([
+        DBusInt32(
+            displayConfig.currentLogicalConfiguration(i).offsetX), //offset x
+        DBusInt32(
+            displayConfig.currentLogicalConfiguration(i).offsetY), // offset y
+        DBusDouble(confMonitor.scale ??
+            displayConfig.currentLogicalConfiguration(i).scale), // scale
+        DBusUint32(confMonitor.transform?.index ??
+            displayConfig
+                .currentLogicalConfiguration(i)
+                .orientation), //transform
+        DBusBoolean(confMonitor.primary ??
+            displayConfig.currentLogicalConfiguration(i).primary), // primary
+        // monitors
+        DBusArray(DBusSignature('(ssa{sv})'), [
+          DBusStruct([
+            DBusString(
+                displayConfig.identity(i).connector), // maybe monitorId ?
+            DBusString(confMonitor.monitorModeId), // option selected
+            DBusDict.stringVariant({})
+          ])
+        ]),
+      ]));
+    }
+
+    await _displayDBusService.apply(displayConfig.serial,
+        ConfigurationMethod.persistent, logicalParameterValues);
+  }
+
+  Future<DisplaysConfiguration> _loadState({required bool notifyStream}) {
+    final Future<DisplaysConfiguration> future = _displayDBusService
+        .getCurrent()
+        .then(_mapToModel)
+        .then((value) => latest = value);
+
+    if (notifyStream) {
+      future.then(_monitorStateStreamController.add);
+    }
+    return future;
+  }
+
+  DisplaysConfiguration _mapToModel(DBusDisplaysConfig dbusConfiguration) {
+    final int monitorsCount = dbusConfiguration.monitorsLength;
+    final List<DisplayMonitorConfiguration> confs = [];
+
+    for (int i = 0; i < monitorsCount; i++) {
+      /// map data only if there's a current option
+      /// if no current option
+      ///   => monitor not used
+      ///   => monitor ignored and not displayed
+      if(dbusConfiguration.currentOption(i) != null){
+        confs.add(
+            DisplayMonitorConfiguration.newConstructor(dbusConfiguration, i));
+      }
+    }
+
+    return DisplaysConfiguration(configurations: confs);
+  }
+}

commit 7897735e6fcc35921574dad539c5232c11292137

BluetoothPage Regression: do not dispose in view
Regression introduced in #295

lib/view/pages/bluetooth/bluetooth_page.dart
@@ -40,13 +40,6 @@ class _BluetoothPageState extends State<BluetoothPage> {
   }
 
   @override
-  void dispose() {
-    final model = context.read<BluetoothModel>();
-    model.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final model = context.watch<BluetoothModel>();
     return YaruPage(