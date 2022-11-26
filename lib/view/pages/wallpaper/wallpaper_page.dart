89c00953086400c30b23395417de994230bea666
Youssef Lasheen <youssef@lasheen.dev>
2022-11-26 19:53:11 +0200

    Wallpaper page: set preview fit device aspect ratio (#408)

--- lib/view/pages/wallpaper/wallpaper_page.dart
+++ lib/view/pages/wallpaper/wallpaper_page.dart
@@ -6,7 +6,9 @@ import 'package:provider/provider.dart';
 import 'package:settings/constants.dart';
 import 'package:settings/l10n/l10n.dart';
 import 'package:settings/services/settings_service.dart';
+import 'package:settings/services/display/display_service.dart';
 import 'package:settings/utils.dart';
+import 'package:settings/view/pages/displays/displays_configuration.dart';
 import 'package:settings/view/pages/settings_page.dart';
 import 'package:settings/view/pages/wallpaper/color_shading_option_row.dart';
 import 'package:settings/view/pages/wallpaper/wallpaper_model.dart';
@@ -37,6 +39,22 @@ class WallpaperPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final model = context.watch<WallpaperModel>();
+    final displayService = context.watch<DisplayService>();
+
+    double getAspectRatio() {
+      DisplaysConfiguration? displaysConfiguration =
+          displayService.currentNotifier.value;
+      if (displaysConfiguration != null) {
+        final resolution =
+            displaysConfiguration.configurations.first.resolution.split('x');
+        double aspectRatio =
+            (int.parse(resolution.first) / int.parse(resolution.last));
+        return aspectRatio;
+      }
+      //Default aspect ratio
+      return 16 / 9;
+    }
+
     const headlineInsets =
         EdgeInsets.only(top: 30, left: 10, right: 10, bottom: 10);
 
@@ -72,17 +90,20 @@ class WallpaperPage extends StatelessWidget {
         ),
         SizedBox(
           width: kDefaultWidth,
-          child: pictureUri.isEmpty
-              ? ChangeNotifierProvider.value(
-                  value: model,
-                  child: const _ColoredBackground(),
-                )
-              : YaruSelectableContainer(
-                  child: _WallpaperImage(
-                    path: pictureUri.replaceAll(gnomeWallpaperSuffix, ''),
+          child: AspectRatio(
+            aspectRatio: getAspectRatio(),
+            child: pictureUri.isEmpty
+                ? ChangeNotifierProvider.value(
+                    value: model,
+                    child: const _ColoredBackground(),
+                  )
+                : YaruSelectableContainer(
+                    child: _WallpaperImage(
+                      path: pictureUri.replaceAll(gnomeWallpaperSuffix, ''),
+                    ),
+                    selected: false,
                   ),
-                  selected: false,
-                ),
+          ),
         ),
         if (model.wallpaperMode == WallpaperMode.solid)
           ColorShadingOptionRow(
@@ -222,7 +243,7 @@ class _WallpaperImage extends StatelessWidget {
     return Image.file(
       File(path),
       filterQuality: FilterQuality.none,
-      fit: BoxFit.fill,
+      fit: BoxFit.cover,
       cacheHeight: height,
     );
   }
